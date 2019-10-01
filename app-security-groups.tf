6c7a2270e453b69fcc1adb38ffdc274f8fd479c2
Jason Stedman <namdets@gmail.com>
2019-10-01 12:53:05 -0400

    added egress for app security group while in dev

--- app-security-groups.tf
+++ app-security-groups.tf
@@ -57,6 +57,13 @@ resource "aws_security_group" "inbound-app-from-lma-for-dev-only" {
     ]
   }
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Owner       = "Avillach_Lab"
     Environment = "development"
