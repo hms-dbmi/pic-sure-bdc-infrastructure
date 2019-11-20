6532dc2d9bd091ec50e855ed3edc9da580d56927
Jason Stedman <namdets@gmail.com>
2019-11-20 14:16:39 -0500

    fixing wildfly start command

--- app-infrastructure/wildfly-scripts/wildfly-user_data.sh
+++ app-infrastructure/wildfly-scripts/wildfly-user_data.sh
@@ -2,13 +2,6 @@
 echo "user-data progress starting update"
 sudo yum -y update 
 echo "user-data progress finished update installing epel-release"
-sudo yum -y install epel-release 
-echo "user-data progress finished epel-release starting python-pip"
-sudo yum -y install python-pip 
-sudo pip install --upgrade pip
-echo "user-data progress finished python-pip starting docker-compose"
-yes | sudo pip install --ignore-installed requests docker-compose
-echo "user-data progress finished docker-compose adding docker-ce repo"
 sudo yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 echo "user-data progress added docker-ce repo starting docker install"
 sudo yum -y install docker-ce docker-ce-cli containerd.io
@@ -24,9 +17,4 @@ sudo aws s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/je
 
 WILDFLY_IMAGE=`sudo docker load pic-sure-wildfly.tar.gz | cud -d ' ' -f 3`
 JAVA_OPTS="-Xms1024m -Xmx2g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
-sudo docker run \ 
-    -v /var/log/wildfly-docker-logs/:/opt/jboss/wildfly/standalone/logs \
-    -v /var/log/wildfly-docker-os-logs/:/var/log \
-    -p 8080:8080 \
-    -d \
-    $WILDFLY_IMAGE
+sudo docker run -v /var/log/wildfly-docker-logs/:/opt/jboss/wildfly/standalone/logs -v /var/log/wildfly-docker-os-logs/:/var/log -p 8080:8080 -d $WILDFLY_IMAGE -e JAVA_OPTS=$JAVA_OPTS $WILDFLY_IMAGE
