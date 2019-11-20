4f3fc736fe43ce600038ad242cb011ea07e7805a
Jason Stedman <namdets@gmail.com>
2019-11-20 14:53:35 -0500

    typos

--- app-infrastructure/wildfly-scripts/wildfly-user_data.sh
+++ app-infrastructure/wildfly-scripts/wildfly-user_data.sh
@@ -9,12 +9,12 @@ echo "user-data progress finished docker install enabling docker service"
 sudo systemctl enable docker
 echo "user-data progress finished enabling docker service starting docker"
 sudo service docker start
-sudo cd /home/centos/
-sudo mkdir wildfly
-sudo cd wildfly
+sudo mkdir /home/centos/wildfly
+sudo chown centos:centos -R /home/centos
+cd /home/centos/wildfly
 
 sudo aws s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_stack_githash/pic-sure-wildfly.tar.gz .
 
-WILDFLY_IMAGE=`sudo docker load pic-sure-wildfly.tar.gz | cud -d ' ' -f 3`
+WILDFLY_IMAGE=`sudo docker load pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
 JAVA_OPTS="-Xms1024m -Xmx2g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
 sudo docker run -v /var/log/wildfly-docker-logs/:/opt/jboss/wildfly/standalone/logs -v /var/log/wildfly-docker-os-logs/:/var/log -p 8080:8080 -d $WILDFLY_IMAGE -e JAVA_OPTS=$JAVA_OPTS $WILDFLY_IMAGE
