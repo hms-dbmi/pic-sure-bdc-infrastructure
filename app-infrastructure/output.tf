5215290d95cc19f2a49bb472a631ccf0ad5389d1
Jason Stedman <namdets@gmail.com>
2019-11-20 18:17:30 -0500

    missed a couple resource refs

--- app-infrastructure/output.tf
+++ app-infrastructure/output.tf
@@ -1,17 +1,17 @@
-output "cluster_endpoint" {
-  value = join("", aws_rds_cluster.aurora-db-cluster.*.endpoint)
-}
- 
-output "all_instance_endpoints_list" {
-  value = [concat(
-    aws_rds_cluster_instance.aurora-cluster-instance.*.endpoint,
-  )]
-}
- 
-output "reader_endpoint" {
-  value = join("", aws_rds_cluster.aurora-db-cluster.*.reader_endpoint)
-}
- 
-output "cluster_identifier" {
-  value = join("", aws_rds_cluster.aurora-db-cluster.*.id)
-}
+#output "cluster_endpoint" {
+#  value = join("", aws_rds_cluster.aurora-db-cluster.*.endpoint)
+#}
+# 
+#output "all_instance_endpoints_list" {
+#  value = [concat(
+#    aws_rds_cluster_instance.aurora-cluster-instance.*.endpoint,
+#  )]
+#}
+# 
+#output "reader_endpoint" {
+#  value = join("", aws_rds_cluster.aurora-db-cluster.*.reader_endpoint)
+#}
+# 
+#output "cluster_identifier" {
+#  value = join("", aws_rds_cluster.aurora-db-cluster.*.id)
+#}
