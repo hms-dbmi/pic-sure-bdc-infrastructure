f7e73676d0ca02e310c2afb20c1e0ad927335a29
Jason Stedman <namdets@gmail.com>
2019-11-20 18:36:08 -0500

    typo

--- app-infrastructure/wildfly-scripts/wildfly-user_data.sh
+++ app-infrastructure/wildfly-scripts/wildfly-user_data.sh
@@ -16,6 +16,6 @@ sudo yum -y install python3-pip
 sudo pip3 install --no-input awscli --upgrade
 sudo aws s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-wildfly.tar.gz .
 
-WILDFLY_IMAGE=`sudo docker load pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
+WILDFLY_IMAGE=`sudo docker load < pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
 JAVA_OPTS="-Xms1024m -Xmx2g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
 sudo docker run -v /var/log/wildfly-docker-logs/:/opt/jboss/wildfly/standalone/logs -v /var/log/wildfly-docker-os-logs/:/var/log -p 8080:8080 -d $WILDFLY_IMAGE -e JAVA_OPTS="$JAVA_OPTS" $WILDFLY_IMAGE
