75b27d7672cd8c9fc35cfe3716d11f633c74f9bd
Jason Stedman <namdets@gmail.com>
2019-11-20 20:27:18 -0500

    chopped off too much

--- app-infrastructure/wildfly-scripts/wildfly-user_data.sh
+++ app-infrastructure/wildfly-scripts/wildfly-user_data.sh
@@ -18,4 +18,4 @@ sudo /usr/local/bin/aws s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-build
 
 WILDFLY_IMAGE=`sudo docker load < pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
 JAVA_OPTS="-Xms1024m -Xmx2g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
-sudo docker run -v /var/log/wildfly-docker-logs/:/opt/jboss/wildfly/standalone/logs -v /var/log/wildfly-docker-os-logs/:/var/log -p 8080:8080 -e JAVA_OPTS="$JAVA_OPTS" $WILDFLY_IMAGE
+sudo docker run -v /var/log/wildfly-docker-logs/:/opt/jboss/wildfly/standalone/logs -v /var/log/wildfly-docker-os-logs/:/var/log -p 8080:8080 -e JAVA_OPTS="$JAVA_OPTS" -d $WILDFLY_IMAGE
