168ff7826d590291a96cabf1cf7077963d23821e
Jason Stedman <namdets@gmail.com>
2019-12-02 12:53:17 -0500

    remember to retrieve artifacts

--- app-infrastructure/scripts/hpds-user_data.sh
+++ app-infrastructure/scripts/hpds-user_data.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
-#echo "user-data progress starting update"
-#sudo yum -y update 
-#echo "user-data progress finished update installing epel-release"
-#sudo yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#echo "user-data progress added docker-ce repo starting docker install"
-#sudo yum -y install docker-ce docker-ce-cli containerd.io
-#echo "user-data progress finished docker install enabling docker service"
-#sudo systemctl enable docker
-#echo "user-data progress finished enabling docker service starting docker"
-#sudo service docker start
-#sudo mkdir /home/centos/hpds
-#sudo chown centos:centos -R /home/centos
-#cd /home/centos/hpds
-#sudo yum -y install python3-pip
-#sudo pip3 install --no-input awscli --upgrade
-#sudo /usr/local/bin/aws s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-hpds.tar.gz .
+sudo /usr/local/bin/aws s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-hpds.tar.gz .
 
 HPDS_IMAGE=`sudo docker load < pic-sure-hpds.tar.gz | cut -d ' ' -f 3`
 ENTRY_POINT='java -XX:+UseParallelGC -XX:SurvivorRatio=250 -Xms1g -Xmx4g -server -jar hpds.jar -httpPort 8080 -DCACHE_SIZE=10000 -DSMALL_TASK_THREADS=1 -DLARGE_TASK_THREADS=1 -DSMALL_JOB_LIMIT=100 -DID_BATCH_SIZE=2000 "-DALL_IDS_CONCEPT=NONE"  "-DID_CUBE_NAME=NONE" > /var/log/hpds.log'
--- app-infrastructure/scripts/wildfly-user_data.sh
+++ app-infrastructure/scripts/wildfly-user_data.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
-#echo "user-data progress starting update"
-#sudo yum -y update 
-#echo "user-data progress finished update installing epel-release"
-#sudo yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#echo "user-data progress added docker-ce repo starting docker install"
-#sudo yum -y install docker-ce docker-ce-cli containerd.io
-#echo "user-data progress finished docker install enabling docker service"
-#sudo systemctl enable docker
-#echo "user-data progress finished enabling docker service starting docker"
-#sudo service docker start
-#sudo mkdir /home/centos/wildfly
-#sudo chown centos:centos -R /home/centos
-#cd /home/centos/wildfly
-#sudo yum -y install python3-pip
-#sudo pip3 install --no-input awscli --upgrade
-#sudo /usr/local/bin/aws s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-wildfly.tar.gz .
+sudo /usr/local/bin/aws s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-wildfly.tar.gz .
 
 WILDFLY_IMAGE=`sudo docker load < pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
 JAVA_OPTS="-Xms1024m -Xmx2g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
