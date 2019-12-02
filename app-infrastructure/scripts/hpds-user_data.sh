552f7db5ae1b1c7f79f9db2fec3e84481848819f
Jason Stedman <namdets@gmail.com>
2019-12-02 15:24:09 -0500

    getting connection timeout on s3... retry and wait a bit to see if it just takes AWS some time to set up the routes

--- app-infrastructure/scripts/hpds-user_data.sh
+++ app-infrastructure/scripts/hpds-user_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-hpds.tar.gz .
+for i in 1 2 3; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-hpds.tar.gz . && break || sleep 30; done
 
 HPDS_IMAGE=`sudo docker load < pic-sure-hpds.tar.gz | cut -d ' ' -f 3`
 ENTRY_POINT='java -XX:+UseParallelGC -XX:SurvivorRatio=250 -Xms1g -Xmx4g -server -jar hpds.jar -httpPort 8080 -DCACHE_SIZE=10000 -DSMALL_TASK_THREADS=1 -DLARGE_TASK_THREADS=1 -DSMALL_JOB_LIMIT=100 -DID_BATCH_SIZE=2000 "-DALL_IDS_CONCEPT=NONE"  "-DID_CUBE_NAME=NONE" > /var/log/hpds.log'
--- app-infrastructure/scripts/wildfly-user_data.sh
+++ app-infrastructure/scripts/wildfly-user_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-wildfly.tar.gz .
+for i in 1 2 3; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-wildfly.tar.gz . && break || sleep 30; done
 
 WILDFLY_IMAGE=`sudo docker load < pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
 JAVA_OPTS="-Xms1024m -Xmx2g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
