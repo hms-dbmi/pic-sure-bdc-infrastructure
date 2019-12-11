e9b5a1850ac0af00d2f52de4ac973e260578cf47
Jason Stedman <namdets@gmail.com>
2019-12-11 14:27:31 -0500

    adding single rds mysql instance

---
+++ app-infrastructure/picsure-db.tf
@@ -0,0 +1,20 @@
+resource "aws_db_instance" "pic-sure-mysql" {
+  allocated_storage = 50
+  storage_type = "hp2"
+  engine = "mysql"
+  engine_version = "5.7"
+  instance_class = "db.t3.small"
+  name = "picsure"
+  username = "root"
+  password = "password"
+  parameter_group_name = "default.mysql5.7"
+  storage_encrypted = true
+  db_subnet_group_name = "main-b"
+  copy_tags_to_snapshot = true
+  
+  tags = {
+    Owner       = "Avillach_Lab"
+    Environment = "development"
+    Name        = "FISMA Terraform Playground - ${var.stack_githash} - PIC-SURE DB Instance"
+  }
+}
