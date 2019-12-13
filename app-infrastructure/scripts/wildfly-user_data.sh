0abb4e2e1779fb5cb5e2bb7b87d5331c01d8e152
Jason Stedman <namdets@gmail.com>
2019-12-13 10:07:21 -0500

    fixing path of standalone download so it is predictable

--- app-infrastructure/scripts/wildfly-user_data.sh
+++ app-infrastructure/scripts/wildfly-user_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for i in 1 2 3; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-wildfly.tar.gz . && break || sleep 30; done
-for i in 1 2 3; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/configs/jenkins_pipeline_build_${stack_githash}/standalone.xml . && break || sleep 30; done
+for i in 1 2 3; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/configs/jenkins_pipeline_build_${stack_githash}/standalone.xml /home/centos/standalone.xml && break || sleep 30; done
 
 WILDFLY_IMAGE=`sudo docker load < pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
 JAVA_OPTS="-Xms1024m -Xmx2g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
