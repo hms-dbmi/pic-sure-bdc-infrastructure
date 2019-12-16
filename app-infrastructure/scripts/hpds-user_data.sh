d94c111ee4918801e5abb59c1d323cdacc724f80
Jason Stedman <namdets@gmail.com>
2019-12-16 18:37:05 -0500

    cranking up the sleep times to 45 seconds waiting for s3 connectivity after startup... not sure why it takes so long

--- app-infrastructure/scripts/hpds-user_data.sh
+++ app-infrastructure/scripts/hpds-user_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-hpds.tar.gz . && break || sleep 30; done
+for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://avillach-datastage-pic-sure-jenkins-dev-builds-3/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-hpds.tar.gz . && break || sleep 45; done
 mkdir -p /opt/local/hpds/all
-for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp ${dataset_s3_url} /opt/local/hpds/hpds_data.tar.gz && break || sleep 30; done
+for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp ${dataset_s3_url} /opt/local/hpds/hpds_data.tar.gz && break || sleep 45; done
 cd /opt/local/hpds
 tar -xvzf hpds_data.tar.gz
 cd ~
