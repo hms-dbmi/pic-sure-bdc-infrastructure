5199a9b335fdce657d45acdf490c483e75e92da0
Jason Stedman <namdets@gmail.com>
2019-12-16 22:45:28 -0500

    opening 443

--- app-infrastructure/edge-security-groups.tf
+++ app-infrastructure/edge-security-groups.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "inbound-from-public-internet" {
       "0.0.0.0/32"
     ]
   }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/32"
+    ]
+  }
 
   tags = {
     Owner       = "Avillach_Lab"
