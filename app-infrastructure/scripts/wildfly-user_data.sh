ab3062fc99f1ffb5107de92778a0dcc1dfcd217e
Jason Stedman <namdets@gmail.com>
2020-01-10 09:44:48 -0500

    adding ssmagent to userdata

--- app-infrastructure/scripts/wildfly-user_data.sh
+++ app-infrastructure/scripts/wildfly-user_data.sh
@@ -1,4 +1,90 @@
-#!/usr/bin/env bash
+#!/bin/bash
+sudo yum install wget -y
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U amazon-cloudwatch-agent.rpm
+sudo touch /opt/aws/amazon-cloudwatch-agent/etc/custom_config.json
+echo "
+
+{
+	\"metrics\": {
+		
+		\"metrics_collected\": {
+			\"cpu\": {
+				\"measurement\": [
+					\"cpu_usage_idle\",
+					\"cpu_usage_user\",
+					\"cpu_usage_system\"
+				],
+				\"metrics_collection_interval\": 300,
+				\"totalcpu\": false
+			},
+			\"disk\": {
+				\"measurement\": [
+					\"used_percent\"
+				],
+				\"metrics_collection_interval\": 600,
+				\"resources\": [
+					\"*\"
+				]
+			},
+			\"mem\": {
+				\"measurement\": [
+					\"mem_used_percent\",
+                                        \"mem_available\",
+                                        \"mem_available_percent\",
+                                       \"mem_total\",
+                                        \"mem_used\"
+                                        
+				],
+				\"metrics_collection_interval\": 600
+			}
+		}
+	},
+	\"logs\":{
+   \"logs_collected\":{
+      \"files\":{
+         \"collect_list\":[
+            {
+               \"file_path\":\"/var/log/secure\",
+               \"log_group_name\":\"secure\",
+               \"log_stream_name\":\"{instance_id} secure\",
+               \"timestamp_format\":\"UTC\"
+            },
+            {
+               \"file_path\":\"/var/log/messages\",
+               \"log_group_name\":\"messages\",
+               \"log_stream_name\":\"{instance_id} messages\",
+               \"timestamp_format\":\"UTC\"
+            },
+						{
+               \"file_path\":\"/var/log/audit/audit.log\",
+               \"log_group_name\":\"audit.log\",
+               \"log_stream_name\":\"{instance_id} audit.log\",
+               \"timestamp_format\":\"UTC\"
+            },
+						{
+               \"file_path\":\"/var/log/yum.log\",
+               \"log_group_name\":\"yum.log\",
+               \"log_stream_name\":\"{instance_id} yum.log\",
+               \"timestamp_format\":\"UTC\"
+            }
+             
+             
+             
+         ]
+      }
+		}
+	}
+
+
+}
+
+" > /opt/aws/amazon-cloudwatch-agent/etc/custom_config.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/custom_config.json  -s
+
 for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://${stack_s3_bucket}/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-wildfly.tar.gz . && break || sleep 45; done
 for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://${stack_s3_bucket}/configs/jenkins_pipeline_build_${stack_githash}/standalone.xml /home/centos/standalone.xml && break || sleep 45; done
 for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://${stack_s3_bucket}/configs/jenkins_pipeline_build_${stack_githash}/pic-sure-schema.sql /home/centos/pic-sure-schema.sql && break || sleep 45; done
@@ -6,7 +92,7 @@ for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://${s
 for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://${stack_s3_bucket}/modules/mysql/mysql-connector-java-5.1.38.jar /home/centos/mysql-connector-java-5.1.38.jar && break || sleep 45; done
 
 sudo docker run  --name schema-init --rm mysql
-sudo docker exec -i mysql -h${mysql-instance-address} -uroot -p${mysql-instance-password} < /home/centos/pic-sure-schema.sql
+sudo docker exec -i mysql -h${mysql-instance-address} -uroot -p${picsure-db-password} < /home/centos/pic-sure-schema.sql
 sudo docker stop schema-init
 
 WILDFLY_IMAGE=`sudo docker load < pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
