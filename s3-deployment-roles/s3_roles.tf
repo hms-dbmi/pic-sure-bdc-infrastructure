28e6a3e8b62d3220a345e902834870fa2b6db12e
Jason Stedman <namdets@gmail.com>
2020-01-16 10:02:09 -0500

    adding fence_mapping.json to allowed downloads

--- s3-deployment-roles/s3_roles.tf
+++ s3-deployment-roles/s3_roles.tf
@@ -39,6 +39,13 @@ resource "aws_iam_role_policy" "wildfly-deployment-s3-policy" {
         "s3:GetObject"
       ],
       "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.stack_s3_bucket}/configs/jenkins_pipeline_build_${var.stack_githash_long}/configs/fence_mapping.json"
+    },
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
       "Resource": "arn:aws:s3:::${var.stack_s3_bucket}/configs/jenkins_pipeline_build_${var.stack_githash_long}/standalone.xml"
     },
     {
