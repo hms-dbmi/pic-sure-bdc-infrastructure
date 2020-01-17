73b72807a238a8783b6a4943b0a8b7d42841dde7
Jason Stedman <namdets@gmail.com>
2020-01-17 11:25:41 -0500

    I now understand, this variable was moved out of the stack_variables but the vars for stack a were not updated yet
    Revert "terraform decided to complain about this being a duplicate declaration without any changes, interesting, true that it was duplicated for the last few days but why did all those deployments succeed in that time?"
    
    This reverts commit 6969a9cdbe8771014b2c61dbc4ff22edffbb3072.

--- app-infrastructure/variables.tf
+++ app-infrastructure/variables.tf
@@ -15,6 +15,11 @@ variable "dataset-s3-object-key" {
   type        = string
 }
 
+variable "ami-id" {
+  description = "AMI to use for all ec2s"
+  type        = string
+}
+
 variable "environment_name" {
   description = "The name of the environment"
   type        = string
