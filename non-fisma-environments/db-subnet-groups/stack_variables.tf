330d3d364f3c96f2ecad52735362355f6d5c134a
Paul Avillach <paul_avillach@hms.harvard.edu>
2020-02-29 18:05:32 -0500

    removing default values

--- non-fisma-environments/db-subnet-groups/stack_variables.tf
+++ non-fisma-environments/db-subnet-groups/stack_variables.tf
@@ -1,18 +1,14 @@
 variable "db-subnet-a-us-east-1a-id" {
   type = string
-  default = "__US_EAST_1A_SUBNET_PROD__"
 }
 variable "db-subnet-a-us-east-1b-id" {
   type = string
-  default = "__US_EAST_1B_SUBNET_PROD__"
 }
 variable "db-subnet-b-us-east-1a-id" {
   type = string
-  default = "__US_EAST_1A_SUBNET_NEXT_PROD__"
 }
 variable "db-subnet-b-us-east-1b-id" {
   type = string
-  default = "__US_EAST_1B_SUBNET_NEXT_PROD__"
 }
 
 
