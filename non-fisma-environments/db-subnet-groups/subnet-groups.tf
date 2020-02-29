0399b4e5571fba14cb4817f5202ffb22160a7681
Paul Avillach <paul_avillach@hms.harvard.edu>
2020-02-29 17:58:42 -0500

    correcting a few typos

--- non-fisma-environments/db-subnet-groups/subnet-groups.tf
+++ non-fisma-environments/db-subnet-groups/subnet-groups.tf
@@ -1,8 +1,8 @@
 
-resource "aws_db_subnet_group" "data-subnet-group" {
-  name       = "main"
+resource "aws_db_subnet_group" "data-subnet-group-a" {
+  name       = "main-a"
   subnet_ids = [
-    var.db-subnet-a-us-east-1a-id 
+    var.db-subnet-a-us-east-1a-id,
     var.db-subnet-a-us-east-1b-id 
   ]
   tags = {
@@ -11,10 +11,10 @@ resource "aws_db_subnet_group" "data-subnet-group" {
     Name        = "FISMA Terraform Playground - DB Subnet Group A"
   }
 }
-resource "aws_db_subnet_group" "data-subnet-group" {
+resource "aws_db_subnet_group" "data-subnet-group-b" {
   name       = "main-b"
   subnet_ids = [
-    var.db-subnet-b-us-east-1a-id 
+    var.db-subnet-b-us-east-1a-id, 
     var.db-subnet-b-us-east-1b-id 
   ]
   tags = {
