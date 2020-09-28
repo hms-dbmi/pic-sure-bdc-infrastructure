9989b2ebf030e3dc0fcfd916e5c6665697abcc44
Jason Stedman <namdets@gmail.com>
2020-09-28 15:40:19 -0400

    adding variable declaration for user_data script

--- app-infrastructure/hpds-instance.tf
+++ app-infrastructure/hpds-instance.tf
@@ -4,6 +4,7 @@ data "template_file" "hpds-user_data" {
   vars = {
     stack_githash = var.stack_githash_long
     dataset_s3_object_key = var.dataset-s3-object-key
+    genomic_dataset_s3_object_key = var.genomic-dataset-s3-object-key
     stack_s3_bucket = var.stack_s3_bucket
   }
 }
