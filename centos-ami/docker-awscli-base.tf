8fffaf66320a541c3d5ffd8ae8348ae3da363f47
Jason Stedman <namdets@gmail.com>
2020-11-18 17:29:33 -0500

    more specific, yet matches more things

--- centos-ami/docker-awscli-base.tf
+++ centos-ami/docker-awscli-base.tf
@@ -84,7 +84,7 @@ data "aws_ami" "cis_benchmark" {
   most_recent = true
   filter {
     name = "name"
-    values = ["CIS CentOS*Linux*7*Benchmark*v*-*Level 1"]
+    values = ["CIS CentOS Linux 7 Benchmark v*"]
   }
 
 
