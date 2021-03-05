3d7d20d8314b9bca6c4346b3dbed37fc67721b64
Jason Stedman <namdets@gmail.com>
2021-03-05 09:54:55 -0500

    adding missing permission for wildfly resource properties

--- s3-deployment-roles/s3_roles.tf
+++ s3-deployment-roles/s3_roles.tf
@@ -205,8 +205,13 @@ resource "aws_iam_role_policy" "httpd-deployment-s3-policy" {
       ],
       "Effect": "Allow",
       "Resource": "arn:aws:s3:::${var.stack_s3_bucket}/data/${var.dataset-s3-object-key}/fence_mapping.json"
-    },
-    {
+    },{
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.stack_s3_bucket}/configs/jenkins_pipeline_build_${var.stack_githash_long}/aggregate-resource.properties"
+    },{
       "Action": [
         "s3:GetObject"
       ],
