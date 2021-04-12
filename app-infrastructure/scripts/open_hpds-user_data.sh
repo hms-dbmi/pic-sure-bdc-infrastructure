1b6403c2b56a5949903ef72c0fa7388b992a94fb
Jason Stedman <namdets@gmail.com>
2021-04-12 10:03:58 -0400

    adding creation of log directory to avoid perms issue

--- app-infrastructure/scripts/open_hpds-user_data.sh
+++ app-infrastructure/scripts/open_hpds-user_data.sh
@@ -193,10 +193,10 @@ cd /opt/local/hpds
 tar -xvzf destigmatized_javabins_rekeyed.tar.gz
 cd ~
 
+sudo mkdir -p /var/log/hpds-docker-logs
 
 for i in 1 2 3 4 5; do sudo /usr/local/bin/aws --region us-east-1 s3 cp s3://${stack_s3_bucket}/domain-join.sh /root/domain-join.sh && break || sleep 45; done
 sudo bash /root/domain-join.sh
-
 
 HPDS_IMAGE=`sudo docker load < /home/centos/pic-sure-hpds.tar.gz | cut -d ' ' -f 3`
 sudo docker run --restart unless-stopped --name=hpds -v /opt/local/hpds:/opt/local/hpds -p 8080:8080 --entrypoint=java -d $HPDS_IMAGE -XX:+UseParallelGC -XX:SurvivorRatio=250 -Xms10g -Xmx40g -server -jar hpds.jar -httpPort 8080 -DCACHE_SIZE=2500 -DSMALL_TASK_THREADS=1 -DLARGE_TASK_THREADS=1 -DSMALL_JOB_LIMIT=100 -DID_BATCH_SIZE=5000 "-DALL_IDS_CONCEPT=NONE"  "-DID_CUBE_NAME=NONE"
