c32e716d9f4d024c71be5d2a1ebf004ccde988dc
TDeSain <thomas.n.desain@gmail.com>
2022-01-05 10:21:25 -0500

    Update docker-awscli-base.tf
    update for centos ami to use latest CIS version

--- centos-ami/docker-awscli-base.tf
+++ centos-ami/docker-awscli-base.tf
@@ -84,7 +84,7 @@ data "aws_ami" "cis_benchmark" {
   most_recent = true
   filter {
     name = "name"
-    values = ["*CIS*Centos*7*Benchmark*"]
+    values = ["CIS CentOS Linux 7 Benchmark v*"]
   }
 
 
