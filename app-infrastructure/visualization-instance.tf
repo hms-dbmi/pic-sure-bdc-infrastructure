b12c6e7f80451e781d21814c3a2190841ad4474d
James <Jamestp19@gmail.com>
2022-04-05 14:37:23 -0400

    Adds more permissions

--- app-infrastructure/visualization-instance.tf
+++ app-infrastructure/visualization-instance.tf
@@ -36,7 +36,9 @@ resource "aws_instance" "visualization-ec2" {
   user_data = data.template_cloudinit_config.visualization-user-data.rendered
 
   vpc_security_group_ids = [
-    aws_security_group.interal_app_traffic.id
+    aws_security_group.outbound-to-trend-micro.id,
+    aws_security_group.interal_app_traffic.id,
+    aws_security_group.inbound-app-from-lma-for-dev-only.id
   ]
   root_block_device {
     delete_on_termination = true
