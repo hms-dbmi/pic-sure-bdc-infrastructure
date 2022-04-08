6781b6e66b4da3bd532175f548ea6d22a41ba281
James <Jamestp19@gmail.com>
2022-04-08 08:53:06 -0400

    Increase wildfly size and instance_type

--- app-infrastructure/wildfly-instance.tf
+++ app-infrastructure/wildfly-instance.tf
@@ -25,7 +25,7 @@ data "template_cloudinit_config" "wildfly-user-data" {
 
 resource "aws_instance" "wildfly-ec2" {
   ami           = var.ami-id
-  instance_type = "m5.large"
+  instance_type = "m5.2xlarge"
 
   associate_public_ip_address = true
 
@@ -46,7 +46,7 @@ resource "aws_instance" "wildfly-ec2" {
   root_block_device {
     delete_on_termination = true
     encrypted             = true
-    volume_size           = 50
+    volume_size           = 100
   }
 
   tags = {
