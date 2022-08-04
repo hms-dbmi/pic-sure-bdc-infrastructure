a45b78320681098169a26bc2421702f3b864aa12
sharra <sharra@mbi-sted-l06787.harvardsecure.wireless.med.harvard.edu>
2022-08-04 17:16:35 -0400

    continued testing

---
+++ genomic-etl-infrastructure/variables.tf
@@ -0,0 +1,54 @@
+variable "stack_githash" {
+  type = string
+}
+variable "stack_githash_long" {
+  type = string
+}
+
+variable "target-stack" {
+  description = "The stack identifier"
+  type        = string
+}
+
+variable "dataset-s3-object-key" {
+  description = "The s3 object key within the environment s3 bucket"
+  type        = string
+}
+
+variable "destigmatized-dataset-s3-object-key" {
+  description = "The s3 object key within the environment s3 bucket"
+  type        = string
+}
+
+variable "genomic-dataset-s3-object-key" {
+  description = "The s3 object key within the environment s3 bucket"
+  type        = string
+}
+
+variable "source_dictionary_s3_object_key" {
+  description = "The s3 object key for the data dictionary"
+  type        = string
+}
+
+variable "ami-id" {
+  description = "AMI to use for all ec2s"
+  type        = string
+}
+
+variable "environment_name" {
+  description = "The name of the environment"
+  type        = string
+  default     = "picsure"
+}
+
+variable "rds_master_username" {
+  description = "Master Username"
+  type        = string
+  default     = "root"
+}
+
+variable "rds_master_password" {
+  description = "Master Password"
+  type        = string
+  default     = "picsure!98765"  
+} 
