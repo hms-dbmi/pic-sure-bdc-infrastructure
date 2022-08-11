e9a22f195bbaa2d9e025f4e9c31c4eed3740db70
sharra <sharra@MBI-STED-L06787.local>
2022-08-11 02:30:48 -0400

    continued testing

--- genomic-etl-infrastructure/variables.tf
+++ genomic-etl-infrastructure/variables.tf
@@ -10,26 +10,6 @@ variable "target-stack" {
   type        = string
 }
 
-variable "dataset-s3-object-key" {
-  description = "The s3 object key within the environment s3 bucket"
-  type        = string
-}
-
-variable "destigmatized-dataset-s3-object-key" {
-  description = "The s3 object key within the environment s3 bucket"
-  type        = string
-}
-
-variable "genomic-dataset-s3-object-key" {
-  description = "The s3 object key within the environment s3 bucket"
-  type        = string
-}
-
-variable "source_dictionary_s3_object_key" {
-  description = "The s3 object key for the data dictionary"
-  type        = string
-}
-
 variable "ami-id" {
   description = "AMI to use for all ec2s"
   type        = string
