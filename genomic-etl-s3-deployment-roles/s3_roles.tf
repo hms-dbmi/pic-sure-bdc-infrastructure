866d7b564facdda6e574633e55c4d02e127f48b2
sharra <sharra@MBI-STED-L06787.local>
2022-08-11 02:56:22 -0400

    continued testing

---
+++ genomic-etl-s3-deployment-roles/s3_roles.tf
@@ -0,0 +1,72 @@
+variable "stack_githash" {
+  type = string
+}
+variable "stack_githash_long" {
+  type = string
+}
+
+variable "target-stack" {
+  description = "The stack identifier"
+  type        = string
+}
+
+
+resource "aws_iam_instance_profile" "genomic-etl-deployment-s3-profile" {
+  name = aws_iam_role.genomic-etl-deployment-s3-role.name
+  role = aws_iam_role.genomic-etl-deployment-s3-role.name
+}
+
+resource "aws_iam_role_policy" "genomic-etl-deployment-s3-policy" {
+  name = "genomic-etl-deployment-s3-profile-${var.target-stack}-${var.stack_githash}"
+  role = aws_iam_role.genomic-etl-deployment-s3-role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:CreateTags"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:ec2:*:*:instance/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "genomic-etl-deployment-s3-role" {
+  name               = "genomic-etl-deployment-s3-profile-${var.target-stack}-${var.stack_githash}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    },
+    {
+      "Action": [
+        "ec2:CreateTags"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:ec2:*:*:instance/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach-cloudwatch-server-policy-to-genomic-etl-role" {
+  role       = aws_iam_role.genomic-etl-deployment-s3-role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "attach-cloudwatch-ssm-policy-to-genomic-etl-role" {
+  role       = aws_iam_role.genomic-etl-deployment-s3-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
