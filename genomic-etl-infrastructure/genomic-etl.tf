c182037f38599249adcce62fd0273e1c4ba59aaa
sharra <sharra@MBI-STED-L06787.local>
2022-08-18 12:53:58 -0400

    testing endpoint connection

--- genomic-etl-infrastructure/genomic-etl.tf
+++ genomic-etl-infrastructure/genomic-etl.tf
@@ -32,7 +32,9 @@ resource "aws_instance" "genomic-etl-ec2" {
   user_data = data.template_cloudinit_config.genomic-user-data.rendered
 
   vpc_security_group_ids = [
-    aws_security_group.traffic-to-ssm.id
+    aws_security_group.traffic-to-ssm.id,
+    aws_security_group.inbound-genomic-from-lma-for-dev-only.id,
+    aws_security_group.inbound-from-public-internet.id
   ]
   root_block_device {
     delete_on_termination = true
