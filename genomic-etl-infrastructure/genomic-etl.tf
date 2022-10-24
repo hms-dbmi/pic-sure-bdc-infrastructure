5ea5d25bb24ec41bfa2384a498c661a3a736bf8a
sharra <sharra@MBI-STED-L06787.hsd1.ma.comcast.net>
2022-10-24 17:25:26 -0400

    tweaks to config files

--- genomic-etl-infrastructure/genomic-etl.tf
+++ genomic-etl-infrastructure/genomic-etl.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "genomic-etl-ec2" {
 
   subnet_id = var.genomic-etl-subnet-us-east-id
 
-  iam_instance_profile = "genomic-etl-deployment-s3-profile-${var.deployment_githash}-{var.study_id}${var.consent_group_tag}-${var.chrom_number}"
+  iam_instance_profile = "genomic-etl-deployment-s3-profile-${var.deployment_githash}-${var.study_id}${var.consent_group_tag}-${var.chrom_number}"
 
   user_data = data.template_cloudinit_config.genomic-user-data.rendered
 
