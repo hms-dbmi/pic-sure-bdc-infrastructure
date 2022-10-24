0476a593a83586217075cc827fa31aec571c96b6
sharra <sharra@MBI-STED-L06787.hsd1.ma.comcast.net>
2022-10-24 17:12:55 -0400

    tweaks to config files

--- genomic-etl-s3-deployment-roles/s3_roles.tf
+++ genomic-etl-s3-deployment-roles/s3_roles.tf
@@ -15,7 +15,7 @@ resource "aws_iam_instance_profile" "genomic-etl-deployment-s3-profile" {
 }
 
 resource "aws_iam_role_policy" "genomic-etl-deployment-s3-policy" {
-  name = "genomic-etl-deployment-s3-profile-${var.deployment_githash}-{var.study_id}${var.consent_group_tag}-${var.chrom_number}"
+  name = "genomic-etl-deployment-s3-profile-${var.deployment_githash}-${var.study_id}${var.consent_group_tag}-${var.chrom_number}"
   role = aws_iam_role.genomic-etl-deployment-s3-role.id
   policy = <<EOF
 {
@@ -34,7 +34,7 @@ EOF
 }
 
 resource "aws_iam_role" "genomic-etl-deployment-s3-role" {
-  name               = "genomic-etl-deployment-s3-profile-${var.deployment_githash}$-{var.study_id}${var.consent_group_tag}-${var.chrom_number} "
+  name               = "genomic-etl-deployment-s3-profile-${var.deployment_githash}-${var.study_id}${var.consent_group_tag}-${var.chrom_number} "
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
