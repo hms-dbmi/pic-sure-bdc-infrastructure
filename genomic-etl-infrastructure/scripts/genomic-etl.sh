a49c4022ed28cd43a4f6aad35861e455416a3000
sharra <sharra@mbi-sted-l06787.harvardsecure.wireless.med.harvard.edu>
2022-11-03 12:22:55 -0400

    established root use for related tasks

--- genomic-etl-infrastructure/scripts/genomic-etl.sh
+++ genomic-etl-infrastructure/scripts/genomic-etl.sh
@@ -99,13 +99,15 @@ mkdir -p /var/log/genomic-docker-logs/ssl_mutex
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")" --silent http://169.254.169.254/latest/meta-data/instance-id)
 sudo /usr/local/bin/aws --region=us-east-1 ec2 create-tags --resources $${INSTANCE_ID} --tags Key=InitComplete,Value=true
 
-sudo /home/centos/ensembl-vep/aws sts assume-role --role-arn ${s3_role} --role-session-name "get-genomic-source-file" > /home/centos/ensembl-vep/assume-role-output.txt
 
-export AWS_ACCESS_KEY_ID=`grep AccessKeyId /home/centos/ensembl-vep/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
-export AWS_SECRET_ACCESS_KEY=`grep SecretAccessKey /home/centos/ensembl-vep/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
-export AWS_SESSION_TOKEN=`grep SessionToken /home/centos/ensembl-vep/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
 
-nohup /home/centos/ensembl-vep/aws s3 cp s3://${input_s3_bucket}/${study_name}_${study_id}_TOPMed_WGS_freeze.9b.chr${chrom_number}.hg38${consent_group_tag}.vcf.gz /home/centos/ensembl-vep/ &
+sudo /usr/local/bin/aws sts assume-role --role-arn ${s3_role} --role-session-name "get-genomic-source-file" > /usr/tmp/assume-role-output.txt
+
+export AWS_ACCESS_KEY_ID=`sudo grep AccessKeyId /usr/tmp/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
+export AWS_SECRET_ACCESS_KEY=`sudo grep SecretAccessKey /usr/tmp/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
+export AWS_SESSION_TOKEN=`sudo grep SessionToken /usr/tmp/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
+
+sudo nohup /usr/local/bin/aws s3 cp s3://${input_s3_bucket}/${study_name}_${study_id}_TOPMed_WGS_freeze.9b.chr${chrom_number}.hg38${consent_group_tag}.vcf.gz /home/centos/ensembl-vep/ &
 
 wait
 
@@ -113,19 +115,19 @@ unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
 
-sudo nohup /home/centos/ensembl-vep/bcftools view -Oz --threads 40 -f PASS,. /home/centos/ensembl-vep/${study_name}_${study_id}_TOPMed_WGS_freeze.9b.chr${chrom_number}.hg38${consent_group_tag}.vcf.gz > /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.filtered.vcf.gz &
+sudo nohup /usr/local/bin/bcftools view -Oz --threads 40 -f PASS,. /home/centos/ensembl-vep/${study_name}_${study_id}_TOPMed_WGS_freeze.9b.chr${chrom_number}.hg38${consent_group_tag}.vcf.gz > /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.filtered.vcf.gz &
 
 wait
 
-sudo nohup /home/centos/ensembl-vep/bcftools annotate --threads 40 --rename-chrs /home/centos/ensembl-vep/chrm_rename.txt ${study_id}${consent_group_tag}.chr${chrom_number}.filtered.vcf.gz | bgzip > /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.renamed.vcf.gz &
+sudo nohup /usr/local/bin/bcftools annotate --threads 40 --rename-chrs /home/centos/ensembl-vep/chrm_rename.txt ${study_id}${consent_group_tag}.chr${chrom_number}.filtered.vcf.gz | bgzip > /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.renamed.vcf.gz &
 
 wait
 
-sudo nohup /home/centos/ensembl-vep/bcftools norm --threads 40 -m -any -f /home/centos/ensembl-vep/Homo_sapiens.GRCh38.dna.primary_assembly.fa -o /home/centos/ensembl-vep/sampleChrm11.normalized.vcf.gz /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.renamed.vcf.gz &
+sudo nohup /usr/local/bin/bcftools norm --threads 40 -m -any -f /home/centos/ensembl-vep/Homo_sapiens.GRCh38.dna.primary_assembly.fa -o /home/centos/ensembl-vep/sampleChrm11.normalized.vcf.gz /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.renamed.vcf.gz &
 
 wait
 
-sudo nohup /home/centos/ensembl-vep/bgzip -d /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.normalized.vcf.gz &                        
+sudo nohup /home/centos/htslib/bgzip -d /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.normalized.vcf.gz &                        
 
 wait
 
@@ -150,29 +152,29 @@ sudo nohup ./home/centos/ensembl-vep/vep \
 
 wait
 
-sudo nohup /home/centos/ensembl-vep/bgzip -fki --threads 40 /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.annotated.vcf &
+sudo nohup /home/centos/htslib/bgzip -fki --threads 40 /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.annotated.vcf &
 
 wait
 
-/home/centos/ensembl-vep/aws sts assume-role --role-arn ${s3_role} --role-session-name "get-genomic-source-file" > /home/centos/ensembl-vep/assume-role-output.txt
+sudo /usr/local/bin/aws sts assume-role --role-arn ${s3_role} --role-session-name "get-genomic-source-file" > /usr/tmp/assume-role-output.txt
 
-export AWS_ACCESS_KEY_ID=`grep AccessKeyId /home/centos/ensembl-vep/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
-export AWS_SECRET_ACCESS_KEY=`grep SecretAccessKey /home/centos/ensembl-vep/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
-export AWS_SESSION_TOKEN=`grep SessionToken /home/centos/ensembl-vep/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
+export AWS_ACCESS_KEY_ID=`sudo grep AccessKeyId /usr/tmp/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
+export AWS_SECRET_ACCESS_KEY=`sudo grep SecretAccessKey /usr/tmp/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
+export AWS_SESSION_TOKEN=`sudo grep SessionToken /usr/tmp/assume-role-output.txt | cut -d ':' -f 2 | sed "s/[ ,\"]//g"`
 
-sudo nohup /home/centos/ensembl-vep/aws s3 cp /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.annotated.vcf.gz s3://${output_s3_bucket}/genomic-etl/processed_vcfs/ &
-
-wait
-
-sudo nohup /home/centos/ensembl-vep/aws s3 cp /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.annotated.vcf.gzi s3://${output_s3_bucket}/genomic-etl/processed_vcfs/ &
+sudo nohup /usr/local/bin/aws s3 cp /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.annotated.vcf.gz s3://${output_s3_bucket}/genomic-etl/processed_vcfs/ &
 
 wait
 
-sudo nohup /home/centos/ensembl-vep/aws s3 cp /home/centos/ensembl-vep/${study_name}_${study_id}_TOPMed_WGS_freeze.9b.chr${chrom_number}.hg38${consent_group_tag}.vcf.gz s3://${output_s3_bucket}/genomic-etl/original_vcfs/${study_id}${consent_group_tag}.chr${chrom_number}.original.vcf.gz &
+sudo nohup /usr/local/bin/aws s3 cp /home/centos/ensembl-vep/${study_id}${consent_group_tag}.chr${chrom_number}.annotated.vcf.gzi s3://${output_s3_bucket}/genomic-etl/processed_vcfs/ &
 
 wait
 
-sudo /home/centos/ensembl-vep/aws s3 cp nohup.out s3://${output_s3_bucket}/genomic-etl/logs/${study_id}${consent_group_tag}.chr${chrom_number}.out
+sudo nohup /usr/local/bin/aws s3 cp /home/centos/ensembl-vep/${study_name}_${study_id}_TOPMed_WGS_freeze.9b.chr${chrom_number}.hg38${consent_group_tag}.vcf.gz s3://${output_s3_bucket}/genomic-etl/original_vcfs/${study_id}${consent_group_tag}.chr${chrom_number}.original.vcf.gz &
+
+wait
+
+sudo /usr/local/bin/aws s3 cp nohup.out s3://${output_s3_bucket}/genomic-etl/logs/${study_id}${consent_group_tag}.chr${chrom_number}.out
 
 wait
 
