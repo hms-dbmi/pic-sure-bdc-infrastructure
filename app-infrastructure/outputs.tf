cdc8983cd97ee71d3a792e90bee9710a66d9726a
bp85 <bhanu_prasad@hms.harvard.edu>
2023-08-11 00:14:37 -0400

    use output

---
+++ app-infrastructure/outputs.tf
@@ -0,0 +1,19 @@
+output "httpd-ec2-id" {
+  value = aws_instance.httpd-ec2.id
+}
+
+output "wildfly-ec2-id" {
+  value = aws_instance.wildfly-ec2.id
+}
+
+output "dictionary-ec2-id" {
+  value = aws_instance.dictionary-ec2.id
+}
+
+output "open-hpds-ec2-id" {
+  value = aws_instance.open-hpds-ec2.id
+}
+
+output "auth-hpds-ec2-id" {
+  value = aws_instance.auth-hpds-ec2.id
+}
