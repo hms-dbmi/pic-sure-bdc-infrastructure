6fbc4835b774d93f455aa98e0345cab2a7fb5bcc
gcolon021 <gcolon021@gmail.com>
2023-09-14 16:04:11 -0400

    [ALS-4998] Fix outputs.tf

--- app-infrastructure/outputs.tf
+++ app-infrastructure/outputs.tf
@@ -11,9 +11,9 @@ output "dictionary-ec2-id" {
 }
 
 output "hpds-ec2-open-id" {
-  value = var.include_open_hpds ? aws_instance.auth-hpds-ec2[0].id : ""
+  value = var.include_open_hpds ? aws_instance.open-hpds-ec2[0].id : ""
 }
 
 output "hpds-ec2-auth-id" {
-  value = var.include_auth_hpds ? aws_instance.open-hpds-ec2[0].id : ""
+  value = var.include_auth_hpds ? aws_instance.auth-hpds-ec2[0].id : ""
 }
