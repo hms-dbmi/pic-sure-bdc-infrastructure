d7cd8fe8006be20172e22e0e97db45ac68e82a8d
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2023-09-20 14:44:15 -0400

    Need to use name or arn
    A bit in limbo as tags is not available for aws provider version.
    Updating the provider version breaks out of scope resources.  Should
    switch to tags when provider can be updated and tested properly.
    
    Leaving tag logic here so it's easy to implement when available.

--- app-infrastructure/target-group-attachment.tf
+++ app-infrastructure/target-group-attachment.tf
@@ -2,10 +2,15 @@
 # Use tags for Project and Stack to find correct target group to update
 
 data "aws_lb_target_group" "lb_tg_data" {
-  tags = {
-    Project = local.project
-    Stack	= local.lb_target_stack
-  }
+  # need to use name or arn with current provider
+  name = local.alb_name
+  # cannot use tags with current aws provider.
+  # updating the provider broke other things
+  # implement tags once provider can be updated to latest.
+  #tags = {
+  #  Project = local.project
+  #  Stack	= local.lb_target_stack
+  #}
 }
 
 # use the arn from data resource staging.
@@ -13,25 +18,38 @@ data "aws_lb_target_group" "lb_tg_data" {
 # each stack gets attached to a single tg
 
 resource "aws_lb_target_group_attachment" "stack_lb_tga" {
-  target_group_arn = data.aws_lb_target_group.lb_tg_data.arn
-  target_id        = aws_instance.httpd-ec2.id
-  port             = 443
+  target_group_arn  = data.aws_lb_target_group.lb_tg_data.arn
+  target_id         = aws_instance.httpd-ec2.id
+  port              = 443
   availability_zone = "all"
 }
 
 # used to promote the tga to the live target group.
 # by default deployment should be always deploying to the staging environment.
-# using this
- 
+# this should only be set to true when promoting staging to live.
 variable "is_promote_lb_tg" {
-  type	=	bool
+  type	  =	bool
   default = false
 }
+
+#### Remove these when moving to tags.  
+variable "staging_lb_name: {
+  type = string
+}
+variable "live_lb_name: {
+  type = string
+}
+
+#####
 
 # variable to handle if we are promoting  
 # is_live tag to let us know if the stack is the live server or not.
 
 locals {
   lb_target_stack = var.is_promote_lb_tg ? var.env_live_subdomain : var.env_staging_subdomain
-  is_live = var.is_promote_lb_tg ? true: false
+  is_live         = var.is_promote_lb_tg ? true: false
+  
+  # for use to support not having tags remove when provider is updated
+  alb_name        = var.is_promote_lb_tg ? var.live_lb_name: staging_lb_name
+  
 }
