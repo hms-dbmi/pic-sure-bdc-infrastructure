806b535a30b1f3824387eb47147776912317c85b
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2023-09-20 12:18:14 -0400

    adding filter for target group * attach target group using project and stack tags * create target group attachment resource and add staging ( predev ) instance on deployment. * need a mechanism to handle deployment and environment promotion. * is_promote_lb_tg should be left as default and not defined in the teardown process.  We will always want to deploy to staging. * Next is to deprecate current move dns pointer job and implement updating the aws_lb_target_group_attachment for each stack when staging is promoted to live.

--- app-infrastructure/target-group-attachment.tf
+++ app-infrastructure/target-group-attachment.tf
@@ -7,28 +7,28 @@ data "aws_lb_target_group" "staging" {
   }
   filter = {
     name = "tag:Stack"
-    values = [ var.env_staging_subdomain ]
+    values = [ var.lb_target_stack ]
   }
-  # change this to find the target group dynamically
 }
 
 # use the arn from data resource staging.
 # use the instance-id for the httpd server
-resource "aws_lb_target_group_attachment" "test" {
-  target_group_arn = aws_instance.staging.arn
+# each stack gets attached to a single tg
+resource "aws_lb_target_group_attachment" "stack_lb_tga" {
+  target_group_arn = data.aws_lb_target_group.staging.arn
   target_id        = aws_instance.httpd-ec2.id
   port             = 443
   availability_zone = "all"
 }
 
-variable "lb_tg_arn" {
-  type    = string
+# used to promote the tga to the live target group.
+# by default deployment should be always deploying to the staging environment.
+# using this 
+variable "is_promote_lb_tg" {
+  type	=	bool
+  default = false
 }
 
-variable "lb_tg_name" {
-  type    = string
-}
-
-variable "target_id" {
-  type    = string
+local {
+  lb_target_stack = is_promote_lb_tg ? var.env_live_subdomain : var.env_staging_subdomain
 }
