952b43a3e0787ba78a20c644fd350f078bf36b98
Ryan Amari <ramari16@gmail.com>
2023-09-21 12:08:22 -0400

    ALS-5052: Update hpds scripts to use tomcat docker image

--- app-infrastructure/scripts/auth_hpds-user_data.sh
+++ app-infrastructure/scripts/auth_hpds-user_data.sh
@@ -37,9 +37,8 @@ sudo docker run --name=auth-hpds \
                 --log-driver syslog --log-opt tag=auth-hpds \
                 -v /opt/local/hpds:/opt/local/hpds \
                 -p 8080:8080 \
-                --entrypoint=java -d $HPDS_IMAGE \
-                -XX:+UseParallelGC -XX:SurvivorRatio=250 -Xms10g -Xmx110g -server -jar hpds.jar \
-                -httpPort 8080 -DCACHE_SIZE=2500 -DSMALL_TASK_THREADS=1 -DLARGE_TASK_THREADS=1 -DSMALL_JOB_LIMIT=100 -DID_BATCH_SIZE=5000 "-DALL_IDS_CONCEPT=NONE"  "-DID_CUBE_NAME=NONE"
+                -e CATALINA_OPTS=" -XX:+UseParallelGC -XX:SurvivorRatio=250 -Xms10g -Xmx110g -DCACHE_SIZE=2500 -DSMALL_TASK_THREADS=1 -DLARGE_TASK_THREADS=1 -DSMALL_JOB_LIMIT=100 -DID_BATCH_SIZE=5000 '-DALL_IDS_CONCEPT=NONE'  '-DID_CUBE_NAME=NONE'"  \
+                -d $HPDS_IMAGE
 
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")" --silent http://169.254.169.254/latest/meta-data/instance-id)
 sudo /usr/bin/aws --region=us-east-1 ec2 create-tags --resources $${INSTANCE_ID} --tags Key=InitComplete,Value=true
