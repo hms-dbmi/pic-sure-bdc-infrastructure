d0eef761926fb9718391f26c11d28b3d66fa8902
Samantha <samantha.piatt@childrens.harvard.edu>
2023-09-22 14:23:27 -0400

    [ALS-4731] Add named_dataset table to pic-sure schema (#73)

--- app-infrastructure/configs/pic-sure-schema.sql
+++ app-infrastructure/configs/pic-sure-schema.sql
@@ -59,6 +59,26 @@ LOCK TABLES `query` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `named_dataset`
+--
+
+DROP TABLE IF EXISTS `named_dataset`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `named_dataset` (
+  `uuid` binary(16) NOT NULL,
+  `queryId` binary(16) NOT NULL,
+  `user` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `archived` bit(1) NOT NULL DEFAULT FALSE,
+  `metadata` TEXT,
+  PRIMARY KEY (`uuid`),
+  CONSTRAINT `foreign_queryId` FOREIGN KEY (`queryId`) REFERENCES `query` (`uuid`),
+  CONSTRAINT `unique_queryId_user` UNIQUE (`queryId`, `user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `resource`
 --
 
