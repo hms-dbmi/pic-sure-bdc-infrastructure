06b4bfaf36e2df2dbd0898425d40404c5a1087c8
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2023-10-02 20:21:07 -0400

    bad output?

--- app-infrastructure/outputs.tf
+++ app-infrastructure/outputs.tf
@@ -14,6 +14,6 @@ output "hpds-ec2-open-id" {
   value = var.include_open_hpds ? aws_instance.open-hpds-ec2[0].id : ""
 }
 
-output "hpds-ec2-auth-id" {
-  value = var.include_auth_hpds ? aws_instance.auth-hpds-ec2[0].id : ""
-}
+#output "hpds-ec2-auth-id" {
+#  value = var.include_auth_hpds ? aws_instance.auth-hpds-ec2[0].id : ""
+#}
