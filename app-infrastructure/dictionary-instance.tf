7c02e08cabc1319d0966cbd236509ea646ac2d33
James <Jamestp19@gmail.com>
2023-10-20 11:33:35 -0400

    [ALS-5128] Add fence mapping file to dictionary resource (#98)
    * Add fence-mapping.json to dictionary
    
    * Add dataset-s3-object-key
    
    * Update path

--- app-infrastructure/dictionary-instance.tf
+++ app-infrastructure/dictionary-instance.tf
@@ -4,6 +4,7 @@ data "template_file" "dictionary-user_data" {
   vars = {
     stack_githash = var.stack_githash_long
     stack_s3_bucket = var.stack_s3_bucket
+    dataset_s3_object_key = var.dataset-s3-object-key
     target-stack    = var.target-stack
   }
 }
