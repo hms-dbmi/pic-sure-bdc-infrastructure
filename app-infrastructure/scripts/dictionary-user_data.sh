70db885c779e1dd69ea586ba3e4b2380d7929fcf
James <Jamestp19@gmail.com>
2023-10-20 09:25:26 -0400

    Update fence mapping path

--- app-infrastructure/scripts/dictionary-user_data.sh
+++ app-infrastructure/scripts/dictionary-user_data.sh
@@ -98,7 +98,7 @@ echo "pulled fence mapping"
 #sudo tar -xvzf /usr/local/docker-config/search/dictionary.javabin.tar.gz -C /usr/local/docker-config/search/
 
 DICTIONARY_IMAGE=`sudo docker load < pic-sure-hpds-dictionary-resource.tar.gz | cut -d ' ' -f 3`
-sudo docker run --name=dictionary -v /var/log/dictionary-docker-logs/:/usr/local/tomcat/logs/ -v /home/centos/fence_mapping.json:/usr/local/fence_mapping.json -e CATALINA_OPTS=" -Xms1g -Xmx12g " -p 8080:8080 -d $DICTIONARY_IMAGE
+sudo docker run --name=dictionary -v /var/log/dictionary-docker-logs/:/usr/local/tomcat/logs/ -v /home/centos/fence_mapping.json:/usr/local/docker-config/search/fence_mapping.json -e CATALINA_OPTS=" -Xms1g -Xmx12g " -p 8080:8080 -d $DICTIONARY_IMAGE
 
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")" --silent http://169.254.169.254/latest/meta-data/instance-id)
 sudo /usr/local/bin/aws --region=us-east-1 ec2 create-tags --resources $${INSTANCE_ID} --tags Key=InitComplete,Value=true
