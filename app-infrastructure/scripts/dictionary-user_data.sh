128ccde2e07b1ecdf5bc94402b58e40fa271614d
James <Jamestp19@gmail.com>
2023-10-20 11:33:06 -0400

    [ALS-5128] Add fence mapping file to dictionary resource (#97)
    * Add dataset-s3-object-key
    
    * Add fence mapping

--- app-infrastructure/scripts/dictionary-user_data.sh
+++ app-infrastructure/scripts/dictionary-user_data.sh
@@ -24,6 +24,9 @@ s3_copy() {
 
 s3_copy s3://${stack_s3_bucket}/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-hpds-dictionary-resource.tar.gz /home/centos/pic-sure-hpds-dictionary-resource.tar.gz
 
+s3_copy s3://${stack_s3_bucket}/data/${dataset_s3_object_key}/fence_mapping.json /home/centos/fence_mapping.json
+echo "pulled fence mapping"
+
 sudo mkdir -p /usr/local/docker-config/search/
 sudo mkdir -p /var/log/dictionary-docker-logs
 
@@ -31,6 +34,7 @@ DICTIONARY_IMAGE=`sudo docker load < /home/centos/pic-sure-hpds-dictionary-resou
 sudo docker run --name=dictionary \
                 --log-driver syslog --log-opt tag=dictionary \
                 -v /var/log/dictionary-docker-logs/:/usr/local/tomcat/logs/ \
+                -v /home/centos/fence_mapping.json:/usr/local/docker-config/search/fence_mapping.json \
                 -e CATALINA_OPTS=" -Xms1g -Xmx12g " \
                 -p 8080:8080 -d $DICTIONARY_IMAGE
 
