d8ab1c28a35c8f5e63f62039fe4a0d74a6938465
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2023-11-11 22:30:52 -0500

    no upserts in mysql =(

--- app-infrastructure/configs/resources-registration.sql
+++ app-infrastructure/configs/resources-registration.sql
@@ -8,6 +8,42 @@ The resources are not stored in a dynamic fashion so they need to be upserted wi
 -- We have multiple stacks so we CANNOT use the same UUID for both if they are both reliant on the same RDS
 -- if we ever want to make this compatiable with other interfaces we must make resource configuration more abstract and dynamic. - TD
 /*!40000 ALTER TABLE `resource` DISABLE KEYS */;
-${include_auth_hpds ? "UPSERT INTO `resource` VALUES (0x02E23F52F3544E8B992CD37C8B9BA140,NULL,'http://auth-hpds.${target_stack}.${env_private_dns_name}:8080/PIC-SURE/','Authorized Access HPDS resource','auth-hpds',NULL, NULL, NULL);" : ""}
-UPSERT INTO `resource` VALUES (0x36363664623161342d386538652d3131,NULL,'http://dictionary.${target_stack}.${env_private_dns_name}:8080/dictionary/pic-sure','Dictionary','dictionary',NULL, NULL, NULL);
+${include_auth_hpds ? 
+  "INSERT INTO `resource` VALUES (
+    0x02E23F52F3544E8B992CD37C8B9BA140,
+    NULL,
+    'http://auth-hpds.${target_stack}.${env_private_dns_name}:8080/PIC-SURE/',
+    'Authorized Access HPDS resource',
+    'auth-hpds',
+    NULL,
+    NULL,
+    NULL
+  ) ON DUPLICATE KEY UPDATE `resourceRSPath` = VALUES(`resourceRSPath`);" : ""}
+
+INSERT INTO `resource`
+VALUES (
+  0x36363664623161342d386538652d3131,
+  NULL,
+  'http://dictionary.${target_stack}.${env_private_dns_name}:8080/dictionary/pic-sure',
+  'Dictionary',
+  'dictionary',
+  NULL,
+  NULL,
+  NULL
+) 
+ON DUPLICATE KEY UPDATE
+  `resourceRSPath` = VALUES(`resourceRSPath`);
 /*!40000 ALTER TABLE `resource` ENABLE KEYS */;
+INSERT INTO `resource`
+VALUES (
+  0x02E23F52F3544E8B992CD37C8B9BA140,
+  NULL,
+  'http://auth-hpds.a.dev.authpicsure.srce.hms.harvard.edu:8080/PIC-SURE/',
+  'Authorized Access HPDS resource',
+  'auth-hpds',
+  NULL,
+  NULL,
+  NULL
+) 
+ON DUPLICATE KEY UPDATE
+  targetURL = VALUES(targetURL); 
