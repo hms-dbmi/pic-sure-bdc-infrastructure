1a5f4c67df6fd26a84e76d361cc20560872502b1
ramari16 <ramari16@gmail.com>
2023-11-16 14:07:12 -0500

    Update auth_hpds-user_data.sh

--- app-infrastructure/scripts/auth_hpds-user_data.sh
+++ app-infrastructure/scripts/auth_hpds-user_data.sh
@@ -103,7 +103,7 @@ cd ~
 sudo mkdir -p /var/log/hpds-docker-logs
 
 HPDS_IMAGE=`sudo docker load < /pic-sure-hpds.tar.gz | cut -d ' ' -f 3`
-sudo docker run --name=hpds -v /opt/local/hpds:/opt/local/hpds -p 8080:8080 --entrypoint=java -d $HPDS_IMAGE -XX:+UseParallelGC -XX:SurvivorRatio=250 -Xms1g -Xmx110g -server -jar hpds.jar -httpPort 8080 -DCACHE_SIZE=2500 -DSMALL_TASK_THREADS=1 -DLARGE_TASK_THREADS=1 -DSMALL_JOB_LIMIT=100 -DID_BATCH_SIZE=5000 "-DALL_IDS_CONCEPT=NONE"  "-DID_CUBE_NAME=NONE"
+sudo docker run --name=hpds -v /opt/local/hpds:/opt/local/hpds -p 8080:8080  -p 9000:9000 -e JAVA_OPTIONS="-XX:+UseParallelGC -XX:SurvivorRatio=250 -Xms1g -Xmx110g -Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:9000 -server -jar hpds.jar -httpPort 8080 -DCACHE_SIZE=2500 -DSMALL_TASK_THREADS=1 -DLARGE_TASK_THREADS=1 -DSMALL_JOB_LIMIT=100 -DID_BATCH_SIZE=5000 -DALL_IDS_CONCEPT=NONE -DID_CUBE_NAME=NONE" -d $HPDS_IMAGE
 
 sudo docker logs -f hpds > /var/log/hpds-docker-logs/hpds.log &
 
