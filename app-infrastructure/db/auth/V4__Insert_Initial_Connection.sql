8a8c50c883d1cb03510837364943a5a54a2a26b1
GeorgeC <gcolon021@gmail.com>
2024-03-04 09:29:44 -0500

    [ALS-5082] Add initial roles, privileges, resources, and create initial tables for database
    The commit includes SQL scripts to insert initial roles and privileges in the 'auth' database and to create the initial tables required for 'picsure' and 'auth' databases. Additionally, the commit inserts resources into the 'picsure' database. These updates initialize the foundational data structures needed for the application's function.

---
+++ app-infrastructure/db/auth/V4__Insert_Initial_Connection.sql
@@ -0,0 +1,5 @@
+/*!40000 ALTER TABLE `connection` DISABLE KEYS */;
+INSERT INTO connection
+VALUES (0xD8C456813239437C951D706D5E56CAB8, '${connection_label}', '${connection_id}', '${connection_sub_prefix}|',
+        '[{"label":"Email", "id":"email"}]');
+/*!40000 ALTER TABLE `connection` ENABLE KEYS */;
