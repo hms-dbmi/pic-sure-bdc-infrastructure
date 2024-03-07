09b2685503b125428aa698584a6a5f41679c0465
Gcolon021 <34667267+Gcolon021@users.noreply.github.com>
2024-03-07 05:58:36 -0500

    [ALS-5082] Persist database: Flyway (#126)
    * [ALS-5082] Remove unused code
    
    This code is tracked by version control and I would prefer not to have dead code within the codebase.
    
    * [ALS-5082] Update MySQL connector version in Wildfly module config
    
    The MySQL connector version in the Wildfly configuration file has been updated from 5.1.38 to 8.0.33. This is necessary for keeping with the latest stable version for improved performance and security.
    
    * [ALS-5082] Add initial roles, privileges, resources, and create initial tables for database
    
    The commit includes SQL scripts to insert initial roles and privileges in the 'auth' database and to create the initial tables required for 'picsure' and 'auth' databases. Additionally, the commit inserts resources into the 'picsure' database. These updates initialize the foundational data structures needed for the application's function.
    
    * Refactor DNS records management in terraform
    
    Removed deprecated comments, unused code and the reliance on Jenkins and AWS CLI for managing route53 DNS records in the route53-template.tf file. The refactoring now utilizes Terraform natively, directly handling AWS Route53 changes and improving code clarity.
    
    * [ALS-5082] Update Terraform configuration and application to use persisted database, remove unnecessary vars, use DB username/host/password vars, add resource IDs and optimize AWS instance size and volume. This change allows us to have a persistent database for our application, which retains data across sessions and improves the overall performance and cost-effectiveness of the AWS instance. Various unnecessary variables have been obliterated, further streamlining the configuration process.
    
    * [ALS-5082] Remove resource-registration.sql

--- app-infrastructure/route53-template.tf
+++ app-infrastructure/route53-template.tf
@@ -1,23 +1,3 @@
-# this needs to be refactored.
-# should just make the route53 changes in terraform
-# instead of outputting this file and using jenkins + aws / cli in the Teardown and Rebuild job to make the record changes. Yikes!!
-#data "template_file" "route53-ip-vars" {
-#  template = file("route53-variables.template")
-#  vars = {
-#    pic-sure-mysql-address    = aws_db_instance.pic-sure-mysql.address
-#    wildfly-ec2-private_ip    = aws_instance.wildfly-ec2.private_ip
-#    httpd-ec2-private_ip      = aws_instance.httpd-ec2.private_ip
-#    open-hpds-ec2-private_ip  = aws_instance.open-hpds-ec2.private_ip
-#    auth-hpds-ec2-private_ip  = aws_instance.auth-hpds-ec2.private_ip
-#    dictionary-ec2-private_ip = aws_instance.dictionary-ec2.private_ip
-#  }
-#}
-
-#resource "local_file" "route53-ip-vars-file" {
-#  content  = data.template_file.route53-ip-vars.rendered
-#  filename = "ip-vars.properties"
-#}
-
 # DNS RECORDS for all nodes
 resource "aws_route53_record" "httpd-addr-record" {
   zone_id = var.env_hosted_zone_id
@@ -59,12 +39,4 @@ resource "aws_route53_record" "dictionary-addr-record" {
   type    = "A"
   ttl     = 60
   records = [aws_instance.dictionary-ec2.private_ip]
-}
-
-resource "aws_route53_record" "picsure-db-cname-record" {
-  zone_id = var.env_hosted_zone_id
-  name    = "picsure-db.${var.target_stack}.${var.env_private_dns_name}"
-  type    = "CNAME"
-  ttl     = 60
-  records = [aws_db_instance.pic-sure-mysql.address]
 }
