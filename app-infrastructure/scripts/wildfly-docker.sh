53460ce78bb44b3362613536f2df900eff903f96
GeorgeC <gcolon021@gmail.com>
2024-06-07 13:57:44 -0400

    [ALS-6101] Refactor Wildfly script and add new PSAMA script
    The wildfly-user_data.sh script has been refactored and broken into separate Wildfly and PSAMA scripts. Each Docker container now has its own script, which improves separation of concerns and maintains a modular code base. The refactoring also reduced the initial sleep command from 600 to 300 in the user_data script.

---
+++ app-infrastructure/scripts/wildfly-docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# except values for target_stack and env_private_dns_name as $1 and $2
+target_stack=$1
+env_private_dns_name=$2
+stack_s3_bucket=$3
+stack_githash=$4
+dataset_s3_object_key=$5
+
+s3_copy() {
+  for i in {1..5}; do
+    sudo /usr/bin/aws --region us-east-1 s3 cp $* && break || sleep 30
+  done
+}
+
+s3_copy "s3://${stack_s3_bucket}/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-wildfly.tar.gz" "/home/centos/pic-sure-wildfly.tar.gz"
+s3_copy "s3://${stack_s3_bucket}/configs/jenkins_pipeline_build_${stack_githash}/standalone.xml" "/home/centos/standalone.xml"
+s3_copy "s3://${stack_s3_bucket}/data/${dataset_s3_object_key}/fence_mapping.json" "/home/centos/fence_mapping.json"
+s3_copy "s3://${stack_s3_bucket}/configs/jenkins_pipeline_build_${stack_githash}/aggregate-resource.properties" "/home/centos/aggregate-resource.properties"
+s3_copy "s3://${stack_s3_bucket}/configs/jenkins_pipeline_build_${stack_githash}/visualization-resource.properties" "/home/centos/visualization-resource.properties"
+
+
+WILDFLY_IMAGE=`sudo docker load < /home/centos/pic-sure-wildfly.tar.gz | cut -d ' ' -f 3`
+JAVA_OPTS="-Xms2g -Xmx26g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=1024m -Djava.net.preferIPv4Stack=true -DTARGET_STACK=${target_stack}.${env_private_dns_name}"
+
+sudo docker run -u root --name=wildfly --network=picsure \
+                        --restart unless-stopped \
+                        --log-driver syslog --log-opt tag=wildfly \
+                        -v /var/log/wildfly-docker-logs/:/opt/jboss/wildfly/standalone/log/ \
+                        -v /home/centos/standalone.xml:/opt/jboss/wildfly/standalone/configuration/standalone.xml \
+                        -v /home/centos/fence_mapping.json:/usr/local/docker-config/fence_mapping.json \
+                        -v /home/centos/aggregate-resource.properties:/opt/jboss/wildfly/standalone/configuration/aggregate-data-sharing/pic-sure-aggregate-resource/resource.properties \
+                        -v /var/log/wildfly-docker-os-logs/:/var/log/ \
+                        -v /home/centos/visualization-resource.properties:/opt/jboss/wildfly/standalone/configuration/visualization/pic-sure-visualization-resource/resource.properties \
+                        -p 8080:8080 -e JAVA_OPTS="$JAVA_OPTS" -d $WILDFLY_IMAGE
