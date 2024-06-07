0e6808f3c2f6397b8ec06f7ee62701ccdc7874c5
GeorgeC <gcolon021@gmail.com>
2024-06-07 15:48:25 -0400

    [ALS-6101] Remove unused variables

--- app-infrastructure/variables.tf
+++ app-infrastructure/variables.tf
@@ -185,16 +185,6 @@ variable "picsure_db_host" {
     description = "The host for the picsure db"
 }
 
-variable "picsure_db_password" {
-    type = string
-    description = "The password for the picsure db"
-}
-
-variable "picsure_db_username" {
-    type = string
-    description = "The username for the picsure db"
-}
-
 variable "referer_allowed_domains" {
     type = string
     description = "The referer allowed domains. A regex string to match the referer domain"
