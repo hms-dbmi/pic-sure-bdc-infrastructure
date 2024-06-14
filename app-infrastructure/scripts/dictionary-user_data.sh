f6f3a7d4a513b601c8b4128c4acf2572e733e9fd
GeorgeC <gcolon021@gmail.com>
2024-06-14 12:27:50 -0400

    Refactor dictionary user data script
    Moved the logic for dictionary docker setup to a separate shell script for better modularity. The dictionary-user_data.sh now downloads this new shell script and executes it, while the new shell script, dictionary-docker.sh, includes the logic that was previously in dictionary-user_data.sh.

--- app-infrastructure/scripts/dictionary-user_data.sh
+++ app-infrastructure/scripts/dictionary-user_data.sh
@@ -21,21 +21,12 @@ s3_copy() {
   done
 }
 
-s3_copy s3://${stack_s3_bucket}/releases/pic-sure-hpds-dictionary-resource.tar.gz /home/centos/pic-sure-hpds-dictionary-resource.tar.gz
+# Download the dictionary docker script
+s3_copy s3://${stack_s3_bucket}/configs/dictionary-docker.sh /home/centos/dictionary-docker.sh
 
-s3_copy s3://${stack_s3_bucket}/data/${dataset_s3_object_key}/fence_mapping.json /home/centos/fence_mapping.json
-echo "pulled fence mapping"
+sudo chmod +x /home/centos/dictionary-docker.sh
 
-sudo mkdir -p /usr/local/docker-config/search/
-sudo mkdir -p /var/log/dictionary-docker-logs
+stack_s3_bucket="${stack_s3_bucket}"
+dataset_s3_object_key="${dataset_s3_object_key}"
 
-DICTIONARY_IMAGE=`sudo docker load < /home/centos/pic-sure-hpds-dictionary-resource.tar.gz | cut -d ' ' -f 3`
-sudo docker run --name=dictionary \
-                --log-driver syslog --log-opt tag=dictionary \
-                -v /var/log/dictionary-docker-logs/:/usr/local/tomcat/logs/ \
-                -v /home/centos/fence_mapping.json:/usr/local/docker-config/search/fence_mapping.json \
-                -e CATALINA_OPTS=" -Xms1g -Xmx12g " \
-                -p 8080:8080 -d $DICTIONARY_IMAGE
-
-INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")" --silent http://169.254.169.254/latest/meta-data/instance-id)
-sudo /usr/bin/aws --region=us-east-1 ec2 create-tags --resources $${INSTANCE_ID} --tags Key=InitComplete,Value=true
+sudo /home/centos/dictionary-docker.sh "$stack_s3_bucket" "$dataset_s3_object_key"
