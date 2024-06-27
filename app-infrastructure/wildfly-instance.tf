0dfa20bf6272066dfc95c1f8ec14c015b4f5501a
GeorgeC <gcolon021@gmail.com>
2024-06-27 13:10:12 -0400

    Remove redundant environment variables and files
    psama.env is no longer rendered by terraform on each deployment. Instead it is configured once and left unchanged until necessary.

--- app-infrastructure/wildfly-instance.tf
+++ app-infrastructure/wildfly-instance.tf
@@ -72,30 +72,6 @@ data "template_file" "wildfly-standalone-xml" {
   }
 }
 
-data "template_file" "psama-env" {
-  template = file("configs/psama.env")
-  vars     = {
-    target_stack = var.target_stack
-    fence_client_secret               = var.fence_client_secret
-    fence_client_id                   = var.fence_client_id
-    idp_provider                      = var.idp_provider
-    idp_provider_uri                  = var.idp_provider_uri
-    sp_client_secret                  = var.sp_client_secret
-    client_id                         = var.client_id
-    sp_client_id                      = var.client_id
-    connection_id                     = var.connection_id
-    app_user_secret_name              = var.app_user_secret_name
-    picsure_db_host                   = var.picsure_db_host
-    picsure_client_secret             = var.picsure_client_secret
-    application_id_for_base_query     = var.application_id_for_base_query
-  }
-}
-
-resource "local_file" "psama-env-file" {
-  filename = "psama.env"
-  content  = data.template_file.psama-env.rendered
-}
-
 resource "local_file" "wildfly-standalone-xml-file" {
   content  = data.template_file.wildfly-standalone-xml.rendered
   filename = "standalone.xml"
