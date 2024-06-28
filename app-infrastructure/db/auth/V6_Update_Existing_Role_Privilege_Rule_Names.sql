48ccbf956b0826b97ab0d6c9640be85e7359b9d1
GeorgeC <gcolon021@gmail.com>
2024-06-28 11:27:23 -0400

    [ALS-6103] Revise naming convention in database script
    The commit modifies several SQL update statements in our database script. It changes naming convention from 'FENCE' to 'MANAGED' throughout role, privilege, and access rule tables.

--- app-infrastructure/db/auth/V6_Update_Existing_Role_Privilege_Rule_Names.sql
+++ app-infrastructure/db/auth/V6_Update_Existing_Role_Privilege_Rule_Names.sql
@@ -1,8 +1,9 @@
 # We have migrated our naming convention away from "FENCE_" to a more generalized "MANAGED_".
+
 -- Update roles table
 UPDATE role
 SET name = REPLACE(name, 'FENCE', 'MANAGED')
-WHERE name LIKE 'FENCE%';
+WHERE name LIKE '%FENCE%';
 
 UPDATE role
 SET description = REPLACE(description, 'FENCE', 'MANAGED')
@@ -10,8 +11,8 @@ WHERE description LIKE '%FENCE%';
 
 -- Update privileges table
 UPDATE privilege
-SET name = REPLACE(name, 'PRIV_FENCE', 'PRIV_MANAGED')
-WHERE name LIKE 'PRIV_FENCE%';
+SET name = REPLACE(name, 'FENCE', 'MANAGED')
+WHERE name LIKE '%FENCE%';
 
 UPDATE privilege
 SET description = REPLACE(description, 'FENCE', 'MANAGED')
@@ -20,7 +21,7 @@ WHERE description LIKE '%FENCE%';
 -- Update access rules table
 UPDATE access_rule
 SET name = REPLACE(name, 'FENCE', 'MANAGED')
-WHERE name LIKE 'FENCE%';
+WHERE name LIKE '%FENCE%';
 
 UPDATE access_rule
 SET description = REPLACE(description, 'FENCE', 'MANAGED')
