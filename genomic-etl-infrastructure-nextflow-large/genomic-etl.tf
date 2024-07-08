d6adf0a6f2c5390bd53621e198b5116820903889
sharra44 <sharra_neely@hms.harvard.edu>
2024-07-08 12:34:08 -0400

    switching az

--- genomic-etl-infrastructure-nextflow-large/genomic-etl.tf
+++ genomic-etl-infrastructure-nextflow-large/genomic-etl.tf
@@ -18,7 +18,7 @@ data "template_file" "genomic-user_data" {
 
 locals {
     subid = (var.genomic-etl-subnet-1b-id)
-    az = "us-east-1d"
+    az = "us-east-1b"
     instanceList = [
      {
     "subnetId" = (local.subid)
