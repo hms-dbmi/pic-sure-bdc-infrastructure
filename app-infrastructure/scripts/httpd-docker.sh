6d1f7121e074730152660e84b99ab34271b1b4b5
James <Jamestp19@gmail.com>
2024-08-23 10:38:19 -0400

    init

---
+++ app-infrastructure/scripts/httpd-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+stack_s3_bucket=$1
+
+s3_copy() {
+  for i in {1..5}; do
+    sudo /usr/bin/aws --region us-east-1 s3 cp $* && break || sleep 30
+  done
+}
+
+s3_copy "s3://${stack_s3_bucket}/configs/psama/bdc.env" "/home/centos/.env"
+s3_copy "s3://${stack_s3_bucket}/releases/psama/psama.tar.gz" "/home/centos/psama.tar.gz"
+
+# This script is responsible for starting or updating the psama container
+PSAMA_IMAGE=`sudo docker load < /home/centos/psama.tar.gz | cut -d ' ' -f 3`
+PSAMA_OPTS="-Xms1g -Xmx2g -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=512m -Djava.net.preferIPv4Stack=true"
+
+# Stop and remove the existing psama container if it exists
+sudo docker stop psama || true
+sudo docker rm psama || true
+sudo docker run -u root --name=psama --restart always --network=picsure \
+--env-file /home/centos/psama.env \
+-v /var/log/psama-docker-os-logs/:/var/log/ \
+-v /var/log/psama-docker-logs/:/opt/psama/logs/ \
+-e JAVA_OPTS="$PSAMA_OPTS" \
+--log-driver syslog --log-opt tag=wildfly \
+-v /home/centos/fence_mapping.json:/config/fence_mapping.json \
+-p 8090:8090 \
+-d $PSAMA_IMAGE
