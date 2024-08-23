1d1be0b94129af0a8a49d9e37b25262eeba8f682
GeorgeC <gcolon021@gmail.com>
2024-08-23 15:50:55 -0400

    Fix incorrect parameter in dictionary resource SQL script
    Changed 'dictResourceUUID' to '@dictResourceUUID' in the SQL insert statement to properly reference the variable. This ensures the dictionary resource is created with the correct UUID parameter.

--- app-infrastructure/db/picsure/V3__Create_Dictionary_resource.sql
+++ app-infrastructure/db/picsure/V3__Create_Dictionary_resource.sql
@@ -5,5 +5,5 @@ SET @dictResourceUUID = unhex(REPLACE(UUID(), '-', ''));
 INSERT INTO `resource`
 (uuid, targetURL, resourceRSPath, description, name, token, hidden, metadata)
 VALUES
-    (dictResourceUUID, NULL, 'http://dictionary-api/',
+    (@dictResourceUUID, NULL, 'http://dictionary-api/',
      'Dictionary API', 'dictionary-api', NULL, TRUE, NULL);
