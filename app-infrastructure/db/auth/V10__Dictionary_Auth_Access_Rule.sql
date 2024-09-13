6bd78bd9242c9f3caffbd2b0c2007222dc4943e2
GeorgeC <gcolon021@gmail.com>
2024-09-13 10:29:47 -0400

    Update dictionary access rule in auth DB script

--- app-infrastructure/db/auth/V10__Dictionary_Auth_Access_Rule.sql
+++ app-infrastructure/db/auth/V10__Dictionary_Auth_Access_Rule.sql
@@ -8,13 +8,7 @@ INTO access_rule (
     uuid, name, description, rule, type, value, checkMapKeyOnly, checkMapNode,
     subAccessRuleParent_uuid, isGateAnyRelation, isEvaluateOnlyByGates
 )    VALUES (
-                @allowDictionaryRequests, 'ALLOW_DICTIONARY_REQUESTS', 'Permit requests to dictionary endpoints',
-                '$.[\'Target Service\']', 6, '/proxy/dictionary-api/facets/',
-             0x00, 0x01, NULL, 0x00, 0x00
+                @allowDictionaryRequests, 'AR_DICTIONARY_REQUESTS', 'Permit requests to dictionary endpoints',
+                '$.[\'Target Service\']', 11, '^/proxy/dictionary-api/.*$',
+             0x00, 0x00, NULL, 0x00, 0x00
             );
--- Add that access rule to the PIC_SURE_ANY_QUERY privilege
-SET @uuidPriv = (SELECT uuid FROM privilege WHERE name = 'MANAGED_PRIV_DICTIONARY'); -- FENCE_ was previously replaced with MANAGED_
-INSERT
-INTO accessRule_privilege (privilege_id, accessRule_id)
-VALUES
-    (@uuidPriv, @allowDictionaryRequests);
