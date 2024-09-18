2e483ac0eab05e7f07f462e45d88312eb4a6d551
GeorgeC <gcolon021@gmail.com>
2024-09-18 11:33:13 -0400

    Stop and remove existing 'httpd' container before run
    Added commands to stop and remove the existing 'httpd' container in the httpd-docker.sh script. This ensures that the docker run command starts a fresh instance without conflicts from previous containers.

--- app-infrastructure/scripts/httpd-docker.sh
+++ app-infrastructure/scripts/httpd-docker.sh
@@ -6,6 +6,8 @@ stack_githash=$2
 s3_copy s3://${stack_s3_bucket}/releases/jenkins_pipeline_build_${stack_githash}/pic-sure-frontend.tar.gz /home/centos/pic-sure-frontend.tar.gz
 
 HTTPD_IMAGE=`sudo docker load < /home/centos/pic-sure-frontend.tar.gz | cut -d ' ' -f 3`
+sudo docker stop httpd || true
+sudo docker rm httpd || true
 sudo docker run --name=httpd \
                 --restart unless-stopped \
                 --log-driver syslog --log-opt tag=httpd \
