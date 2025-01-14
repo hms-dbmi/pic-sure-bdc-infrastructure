9c0465dd8871c5b2ee189b2eb48ad245241d7d87
GeorgeC <gcolon021@gmail.com>
2025-01-14 15:39:39 -0500

    Update RAS connection insert logic with upsert functionality

--- app-infrastructure/db/auth/V11__Insert_RAS_Connection.sql
+++ app-infrastructure/db/auth/V11__Insert_RAS_Connection.sql
@@ -1,3 +1,14 @@
 INSERT INTO connection
-VALUES (0xD8C456813239437C951D706D5E56CAB8, 'RAS', 'ras', 'okta-ras|',
-        '[{"label":"Email", "id":"email"}]');
+(uuid, label, id, subprefix, requiredFields)
+VALUES (
+  UNHEX(REPLACE('9770edc4-1533-4796-9005-5cfb77c0f28d', '-', '')),
+  'RAS',
+  'ras',
+  'okta-ras|',
+  '[{"label":"Email", "id":"email"}]'
+) AS new_vals
+ON DUPLICATE KEY UPDATE
+    label           = new_vals.label,
+    id              = new_vals.id,
+    subprefix       = new_vals.subprefix,
+    requiredFields  = new_vals.requiredFields;
