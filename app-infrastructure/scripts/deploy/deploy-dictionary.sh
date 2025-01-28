c350d06c5c7cdfcd7367e2cfc0ca2281027b3f34
GeorgeC <gcolon021@gmail.com>
2025-01-28 17:55:06 -0500

    Remove unused dataset_s3_object_key variable in deploy script.

--- app-infrastructure/scripts/deploy/deploy-dictionary.sh
+++ app-infrastructure/scripts/deploy/deploy-dictionary.sh
@@ -19,9 +19,8 @@ done
 
 stack_s3_bucket=${stack_s3_bucket:-STACK_S3_BUCKET}
 target_stack=${target_stack:-TARGET_STACK}
-dataset_s3_object_key=${dataset_s3_object_key:-DATASET_S3_OBJECT_KEY}
 
-if [[ -z "$stack_s3_bucket" || -z "$dataset_s3_object_key" || -z "$target_stack" ]]; then
+if [[ -z "$stack_s3_bucket" || -z "$target_stack" ]]; then
   echo "Error: --stack_s3_bucket and --dataset_s3_object_key are required."
   exit 1
 fi
