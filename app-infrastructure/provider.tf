9cb4bda17430061fa46124c1bee9ebf8d3725918
TDeSain <thomas_desain@hms.harvard.edu>
2025-02-27 08:42:54 -0500

    Update and add provider configurations in Terraform (#175)
    Added "template" and "local" providers with specified versions. Updated "random" provider to a newer version. These changes ensure compatibility and extend functionality.

--- app-infrastructure/provider.tf
+++ app-infrastructure/provider.tf
@@ -4,6 +4,13 @@ provider "aws" {
   version = "3.74"
 }
 
+provider "template" {
+  version = "2.2.0"
+}
+
 provider "random" {
-  version = "~> 2.2.1"
+  version = "3.6.3"
+}
+provider "local" {
+  version = "2.5.2"
 }
