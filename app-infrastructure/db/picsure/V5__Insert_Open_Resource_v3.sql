7cd28a73c874d951c19388293dddbe4faf6cd105
Ryan Amari <ramari16@gmail.com>
2025-09-22 14:18:16 -0400

    ALS-9867: Add open resource for new query

---
+++ app-infrastructure/db/picsure/V5__Insert_Open_Resource_v3.sql
@@ -0,0 +1,4 @@
+INSERT INTO `resource`
+VALUES (unhex(REPLACE('ac004461-1b47-4832-80e2-22a4aecabe39', '-', '')), NULL,
+        'http://localhost:8080/pic-sure-aggregate-resource/pic-sure/aggregate-data-sharing-v3',
+        'Open Access (aggregate) resource V3', 'open-hpds', NULL, ${include_open_hpds}, NULL);
