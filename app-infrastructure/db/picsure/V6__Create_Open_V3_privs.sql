e827a7214400e1e4134c65a9f2e57343db9c1bfe
Ryan Amari <ramari16@gmail.com>
2025-09-29 14:55:23 -0400

    ALS-9876: Add open picsure privs

---
+++ app-infrastructure/db/picsure/V6__Create_Open_V3_privs.sql
@@ -0,0 +1,21 @@
+SET
+@uuidAR_OPEN_QUERIES = REPLACE('ac004461-1b47-4832-80e2-22a4aecabe39', '-', '');
+
+INSERT INTO access_rule
+VALUES (unhex(@uuidAR_OPEN_QUERIES),
+        'AR_ALLOW_OPEN_ACCESS_V3',
+        'allow access to open hpds resource',
+        '$.query.resourceUUID',
+        9,
+        '70c837be-5ffc-11eb-ae93-0242ac130002',
+        0,
+        0,
+        NULL,
+        0,
+        0);
+
+set @open_priv_uuid = select uuid from privilege where name = 'FENCE_PRIV_OPEN_ACCESS';
+
+INSERT INTO accessRule_privilege (privilege_id, accessRule_id)
+VALUES (unhex(@open_priv_uuid),
+        unhex(@uuidAR_OPEN_QUERIES));
