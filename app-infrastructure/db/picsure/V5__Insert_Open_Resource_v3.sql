8959e048889f651082d01eba7674dc7cf2969338
ramari16 <ramari16@gmail.com>
2025-10-10 12:33:15 +0200

    ALS-9867: Add resource and permissions for v3 query aggregate resource (#184)

---
+++ app-infrastructure/db/picsure/V5__Insert_Open_Resource_v3.sql
@@ -0,0 +1,4 @@
+INSERT INTO `resource`
+VALUES (unhex(REPLACE('ac004461-1b47-4832-80e2-22a4aecabe39', '-', '')), NULL,
+        'http://localhost:8080/pic-sure-aggregate-resource/pic-sure/v3/aggregate-data-sharing',
+        'Open Access (aggregate) resource V3', 'open-hpds-v3', NULL, ${include_open_hpds}, NULL);
