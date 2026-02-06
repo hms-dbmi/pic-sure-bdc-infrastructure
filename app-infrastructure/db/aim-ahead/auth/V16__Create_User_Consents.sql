c3d72016f5bca06ef42718b8539d9f5556a1f147
Ryan Amari <ramari16@gmail.com>
2026-02-06 13:42:56 -0500

    ALS-9545: Add uuid to user_consents

--- app-infrastructure/db/aim-ahead/auth/V16__Create_User_Consents.sql
+++ app-infrastructure/db/aim-ahead/auth/V16__Create_User_Consents.sql
@@ -1,4 +1,5 @@
 CREATE TABLE user_consents (
+   `uuid` binary(16) NOT NULL UNIQUE,
    `user_id` binary(16) NOT NULL UNIQUE,
    `consents` JSON,
    FOREIGN KEY (`user_id`) REFERENCES user(`uuid`)
