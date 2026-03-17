62fcafe6c258ae742132a5d3436afd5e4d036a66
GeorgeC <gcolon021@gmail.com>
2026-03-17 13:35:40 -0400

    Update NHANES privilege query template and access rules in AIM-AHEAD auth database

--- app-infrastructure/db/aim-ahead/auth/V16_Update_Nhanes_Query_Template.sql
+++ app-infrastructure/db/aim-ahead/auth/V16_Update_Nhanes_Query_Template.sql
@@ -1,3 +1,14 @@
 UPDATE privilege
 SET queryTemplate = '{"categoryFilters": {"\\\\_consents\\\\":["Nhanes"]},"numericFilters":{},"requiredFields":[],"fields":[],"variantInfoFilters":[{"categoryVariantInfoFilters":{},"numericVariantInfoFilters":{}}],"expectedResultType": "COUNT"}'
 WHERE name = "PRIV_MANAGED_nhanes";
+
+update privilege
+SET queryScope = '["\\\\Nhanes\\\\","_"]'
+where name = "PRIV_MANAGED_nhanes";
+
+
+update access_rule set value = '\\\\Nhanes\\\\'
+where value = '\\\\nhanes\\\\';
+
+update access_rule set value = 'Nhanes'
+where value = 'nhanes';
