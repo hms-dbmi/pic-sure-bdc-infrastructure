3507037b1cd8eb304fdaa595f17e8be85636c781
James <Jamestp19@gmail.com>
2026-03-31 10:17:42 -0400

    [ALS-10851] Enable logging deployments and Auth rules (#196)
    Includes 
    ALS-9545: Add roles, privs, access rules for v3 auth
    
    ---------
    
    Co-authored-by: Ryan Amari <ramari16@gmail.com>
    Co-authored-by: GeorgeC <gcolon021@gmail.com>
    Co-authored-by: Gcolon021 <34667267+Gcolon021@users.noreply.github.com>

---
+++ app-infrastructure/db/aim-ahead/picsure/V6__CREATE_LOGGING_RESOURCE.sql
@@ -0,0 +1,4 @@
+INSERT INTO `resource`
+VALUES (unhex(REPLACE('35d190ad-6547-46c5-a7ad-ae35cfd2e34b', '-', '')), NULL,
+        'http://pic-sure-logging/', 'Logging Service',
+        'pic-sure-logging', NULL, true, NULL);
