e6b5576271703609b2546d1470e72935b9543d4e
GeorgeC <gcolon021@gmail.com>
2026-04-09 13:13:21 -0400

    Remove dataset-specific S3 keys from deploy scripts
    Streamlined the deploy-auth-hpds scripts by eliminating dataset-specific S3 object keys and their associated operations. Simplified error handling and variable management for improved maintainability.

--- app-infrastructure/scripts/deploy/deploy-auth-hpds.sh
+++ app-infrastructure/scripts/deploy/deploy-auth-hpds.sh
@@ -6,14 +6,6 @@ while [[ $# -gt 0 ]]; do
       stack_s3_bucket="$2"
       shift 2
       ;;
-   --dataset_s3_object_key)
-      dataset_s3_object_key="$2"
-      shift 2
-      ;;
-    --genomic_dataset_s3_object_key)
-      genomic_dataset_s3_object_key="$2"
-      shift 2
-      ;;
     --environment_name)
       environment_name="$2"
       shift 2
@@ -41,23 +33,17 @@ if [[ -f /etc/environment ]]; then
 fi
 
 stack_s3_bucket=${stack_s3_bucket:-$STACK_S3_BUCKET}
-dataset_s3_object_key=${dataset_s3_object_key:-$DATASET_S3_OBJECT_KEY}
-genomic_dataset_s3_object_key=${genomic_dataset_s3_object_key:-$GENOMIC_DATASET_S3_OBJECT_KEY}
 target_stack=${target_stack:-$TARGET_STACK}
 environment_name=${environment_name:-$ENVIRONMENT_NAME}
 env_private_dns_name=${env_private_dns_name:-$ENV_PRIVATE_DNS_NAME}
 
-
-
-if [[ -z "$stack_s3_bucket" || -z "$genomic_dataset_s3_object_key" || -z "$dataset_s3_object_key" || -z "$environment_name" || -z "$target_stack" || -z "$env_private_dns_name" ]]; then
-  echo "Error: --stack_s3_bucket, --genomic_dataset_s3_object_key, --dataset_s3_object_key, --environment_name, --target_stack, and --env_private_dns_name are required"
+if [[ -z "$stack_s3_bucket" || -z "$environment_name" || -z "$target_stack" || -z "$env_private_dns_name" ]]; then
+  echo "Error: --stack_s3_bucket, --environment_name, --target_stack, and --env_private_dns_name are required"
   exit 1
 fi
 
 cat << EOF > /opt/picsure/deploy-script-last-execution-values.txt
 export stack_s3_bucket="${stack_s3_bucket}"
-export dataset_s3_object_key="${dataset_s3_object_key}"
-export genomic_dataset_s3_object_key="${genomic_dataset_s3_object_key}"
 export environment_name="${environment_name}"
 export target_stack="${target_stack}"
 export env_private_dns_name="${env_private_dns_name}"
@@ -70,13 +56,7 @@ s3_copy() {
 }
 
 s3_copy "s3://${stack_s3_bucket}/${target_stack}/containers/pic-sure-hpds.tar.gz" "/opt/picsure/pic-sure-hpds.tar.gz"
-s3_copy "s3://${stack_s3_bucket}/data/${dataset_s3_object_key}/javabins_rekeyed.tar" "/opt/local/hpds/javabins_rekeyed.tar"
-s3_copy "s3://${stack_s3_bucket}/data/${genomic_dataset_s3_object_key}/all/" "/opt/local/hpds/all/" --recursive
 s3_copy "s3://${stack_s3_bucket}/configs/hpds/${target_stack}/auth-hpds.env" "/opt/picsure/auth-hpds.env"
-
-cd /opt/local/hpds || exit 1
-tar -xvf javabins_rekeyed.tar
-cd ~ || exit 1
 
 chmod 644 /opt/local/hpds/*
 chmod 644 /opt/local/hpds/all/*
