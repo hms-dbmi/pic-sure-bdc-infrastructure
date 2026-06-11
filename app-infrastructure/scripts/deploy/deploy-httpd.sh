4950b3f091ba483269d9732cbfc878c90fde9ee8
GeorgeC <gcolon021@gmail.com>
2026-06-11 11:40:38 -0400

    Update S3 path for `bdc.env` in roles and deploy script
    Updated the S3 path for `bdc.env` to use `pic-sure-frontend` instead of `httpd` in `s3_roles.tf` and `deploy-httpd.sh`. This ensures accurate file references during deployment.

--- app-infrastructure/scripts/deploy/deploy-httpd.sh
+++ app-infrastructure/scripts/deploy/deploy-httpd.sh
@@ -45,7 +45,7 @@ s3_copy() {
 
 s3_copy "s3://${stack_s3_bucket}/${target_stack}/containers/pic-sure-frontend.tar.gz" "/opt/picsure/pic-sure-frontend.tar.gz"
 s3_copy "s3://${stack_s3_bucket}/${target_stack}/configs/httpd/httpd-vhosts.conf" "/usr/local/docker-config/httpd-vhosts.conf"
-s3_copy "s3://${stack_s3_bucket}/configs/httpd/bdc.env" "/opt/picsure/bdc.env"
+s3_copy "s3://${stack_s3_bucket}/configs/pic-sure-frontend/bdc.env" "/opt/picsure/bdc.env"
 s3_copy "s3://${stack_s3_bucket}/certs/httpd/" "/usr/local/docker-config/cert/" --recursive
 
 CONTAINER_NAME=httpd
