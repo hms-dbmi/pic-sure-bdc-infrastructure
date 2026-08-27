488334d13dbf8f4333f3bb364fc9a9133346baee
Ryan Amari <ramari16@gmail.com>
2026-08-27 14:42:15 -0400

    Fix migration script

--- app-infrastructure/db/bdc/auth/V22__Create_User_Consents_Override.sql
+++ app-infrastructure/db/bdc/auth/V22__Create_User_Consents_Override.sql
@@ -1,15 +1,15 @@
 create table consents_override (
-    `uuid` binary(16) NOT NULL UNIQUE,
-    `name` TEXT NOT NULL UNIQUE,
-    `consents` JSON
+   `uuid` binary(16) NOT NULL UNIQUE,
+   `name` varchar(255) COLLATE utf8_bin NOT NULL,
+   `consents` JSON
 );
 
 CREATE TABLE user_consents_override (
-   `uuid` binary(16) NOT NULL UNIQUE,
-   `user_id` binary(16) NOT NULL UNIQUE,
-   `consents_override_id` binary(16) NOT NULL,
-   `enabled` bit(1) NOT NULL DEFAULT b'1',
-   FOREIGN KEY (`user_id`) REFERENCES user(`uuid`)
-       ON DELETE CASCADE,
-   FOREIGN KEY (`consents_override_id`) REFERENCES consents_override(`uuid`)
+    `uuid` binary(16) NOT NULL UNIQUE,
+    `user_id` binary(16) NOT NULL UNIQUE,
+    `consents_override_id` binary(16) NOT NULL,
+    `enabled` bit(1) NOT NULL DEFAULT b'1',
+    FOREIGN KEY (`user_id`) REFERENCES user(`uuid`)
+        ON DELETE CASCADE,
+    FOREIGN KEY (`consents_override_id`) REFERENCES consents_override(`uuid`)
 );
