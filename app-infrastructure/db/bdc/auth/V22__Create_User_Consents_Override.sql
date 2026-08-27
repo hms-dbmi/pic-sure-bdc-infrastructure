717eadc64e31d98f730314e9e20f576f90ef0f70
Ryan Amari <ramari16@gmail.com>
2026-08-27 14:08:15 -0400

    Create consents override table

--- app-infrastructure/db/bdc/auth/V22__Create_User_Consents_Override.sql
+++ app-infrastructure/db/bdc/auth/V22__Create_User_Consents_Override.sql
@@ -1,8 +1,15 @@
+create table consents_override {
+    `uuid` binary(16) NOT NULL UNIQUE,
+    `name` TEXT NOT NULL UNIQUE,
+    `consents` JSON
+};
+
 CREATE TABLE user_consents_override (
    `uuid` binary(16) NOT NULL UNIQUE,
    `user_id` binary(16) NOT NULL UNIQUE,
-   `consents` JSON,
+   `consents_override_id` binary(16) NOT NULL,
    `enabled` bit(1) NOT NULL DEFAULT b'1',
    FOREIGN KEY (`user_id`) REFERENCES user(`uuid`)
-       ON DELETE CASCADE
+       ON DELETE CASCADE,
+   FOREIGN KEY (`consents_override_id`) REFERENCES consents_override(`uuid`)
 );
